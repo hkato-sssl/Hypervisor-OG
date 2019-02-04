8f66e2de3c9a7551a924d43cf6bdea3d91f30eee
Hidekazu Kato <hkato.sssl@gmail.com>
2019-02-04 20:03:05 +0900

    ビルドスクリプト修正
    ・WORKSPACE_LIBRARYの定義に従いインクルードパスを追加
    
    	modified:   script/make/project-env.mk

--- script/make/project-env.mk
+++ script/make/project-env.mk
@@ -4,6 +4,9 @@ SRCDIRS_FILE := .srcdirs
 VARIANT ?= Debug
 
 INC_OPS := $(addprefix -I,$(INCS))
+ifdef WORKSPACE_LIBRARY
+INC_OPS += $(addprefix -I$(WORKSPACE_LOC)/,$(addsuffix /inc,$(WORKSPACE_LIBRARY)))
+endif
 
 ifeq ($(JOBS),)
 JOBS := 1

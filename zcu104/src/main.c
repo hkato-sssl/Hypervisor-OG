617463cf017b11c064d6226d7dace7f1bc29a390
Hidekazu Kato <hkato.sssl@gmail.com>
2019-02-04 17:25:22 +0900

    implementing codes...
    	new file:   src/main.c
    	new file:   src/startup/init_exception_vector.S
    	modified:   src/startup/start.S

---
+++ zcu104/src/main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#include "config/system.h"
+#include <stdint.h>
+
+/* defines */
+
+/* types */
+
+/* variables */
+
+/* functions */
+
+int main(void)
+{
+	return -1;
+}

95d647ade4133d021e21a41d5ea19b40cdda06e1
Hidekazu Kato <hkato.sssl@gmail.com>
2019-02-07 20:08:41 +0900

    implementing the new library.
    	new file:   libs/inc/lib/list.h
    	new file:   libs/src/list.c

---
+++ libs/inc/lib/list.h
@@ -0,0 +1,56 @@
+/*
+ * lib/list.h
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#ifndef LIB_LIST_H
+#define LIB_LIST_H
+
+/* includes */
+
+/* defines */
+
+#ifndef ASSEMBLY
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* includes */
+
+#include <stdint.h>
+#include "lib/system/errno.h"
+
+/* defines */
+
+/* types */
+
+struct list_element {
+    struct list_element *prev;
+    struct list_element *next;
+    void                *value;
+};
+
+struct list {
+    struct list_element *head;
+    struct list_element *tail;
+    uint64_t            num;
+};
+
+/* variables */
+
+/* functions */
+
+errno_t list_init(struct list *list);
+errno_t list_append(struct list *list, struct list_element *e);
+struct list_element *list_get_front(struct list *list);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASSEMBLY */
+
+#endif /* LIB_LIST_H */
+
---
+++ libs/src/list.c
@@ -0,0 +1,78 @@
+/*
+ * lib/list.c
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include "lib/system/errno.h"
+#include "lib/list.h"
+
+/* defines */
+
+/* types */
+
+/* variables */
+
+/* functions */
+
+errno_t list_init(struct list *list)
+{
+    errno_t ret;
+
+    if (list != NULL) {
+        list->head = list->tail = NULL;
+        list->num = 0;
+        ret = SUCCESS;
+    } else {
+        ret = -EINVAL;
+    }
+
+    return ret;
+}
+
+errno_t list_append(struct list *list, struct list_element *e)
+{
+    errno_t ret;
+
+    if ((list != NULL) && (e != NULL)) {
+        if (list->tail == NULL) {
+            e->prev = e->next = NULL;
+            list->head = list->tail = e;
+        } else {
+            e->prev = list->tail;
+            e->next = NULL;
+            list->tail->next = e;
+            list->tail = e;
+        }
+        ++(list->num);
+        ret = SUCCESS;
+    } else {
+        ret = -EINVAL;
+    }
+
+    return ret;
+}
+
+struct list_element *list_get_front(struct list *list)
+{
+    struct list_element *e;
+
+    if (list != NULL) {
+        e = list->head;
+        if (e != NULL) {
+            list->head = e->next;
+            if (e->next != NULL) {
+                e->next->prev = NULL;
+            } else {
+                list->tail = NULL;
+            }
+            --(list->num);
+        }
+    } else {
+        e = NULL;
+    }
+
+    return e;
+}

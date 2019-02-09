a64dd3ec0bee1c2d6a499d93b74b57bf02395c5b
Hidekazu Kato <hkato.sssl@gmail.com>
2019-02-09 20:37:09 +0900

    implementing the cache library.
    	new file:   libs/inc/lib/aarch64/cache.h
    	new file:   libs/src/aarch64/cache/aarch64_dcache_clean.S
    	new file:   libs/src/aarch64/cache/aarch64_dcache_clean_invd.S
    	new file:   libs/src/aarch64/cache/aarch64_dcache_invd.S
    	new file:   libs/src/aarch64/cache/aarch64_icache_invd.S
    	new file:   libs/src/aarch64/cache/aarch64_poc_line_sz.S

---
+++ libs/inc/lib/aarch64/cache.h
@@ -0,0 +1,57 @@
+/*
+ * lib/aarch64/cache.h
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#ifndef LIB_AARCH64_CACHE_H
+#define LIB_AARCH64_CACHE_H
+
+/* includes */
+
+/* defines */
+
+#ifndef ASSEMBLY
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* includes */
+
+/* defines */
+
+/* types */
+
+/* variables */
+
+/* functions */
+
+size_t aarch64_poc_line_sz(void);
+
+void aarch64_icache_invd_all(void);
+void aarch64_icache_invd(void *addr);
+errno_t aarch64_icache_invd_range(void *st, void *ed);
+
+void aarch64_dcache_invd_all(void);
+void aarch64_dcache_invd(void *addr);
+errno_t aarch64_dcache_invd_range(void *st, void *ed);
+
+void aarch64_dcache_clean_all(void);
+void aarch64_dcache_clean(void *addr);
+errno_t aarch64_dcache_clean_range(void *st, void *ed);
+
+void aarch64_dcache_clean_invd_all(void);
+void aarch64_dcache_clean_invd(void *addr);
+errno_t aarch64_dcache_clean_invd_range(void *st, void *ed);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASSEMBLY */
+
+#endif /* LIB_AARCH64_CACHE_H */
+

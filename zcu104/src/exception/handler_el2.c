d3395db6e1f2cc741e96f7c87ba57feff227d76b
Hidekazu Kato <hkato.sssl@gmail.com>
2019-02-22 01:59:29 +0900

    例外処理修正
    例外処理用スタックポインタのアドレスを計算で求める形式に変更
    例外ハンドラ名を変更
    
    	modified:   zcu104/src/exception/excvec_el2.S
    	new file:   zcu104/src/exception/handler_el2.c

---
+++ zcu104/src/exception/handler_el2.c
@@ -0,0 +1,22 @@
+/*
+ * exception/handler_el2.c
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#include <stdint.h>
+
+/* defines */
+
+/* types */
+
+/* prototypes */
+
+/* variables */
+
+/* functions */
+
+void exception_handler_el2(uint64_t *exc)
+{
+}
+

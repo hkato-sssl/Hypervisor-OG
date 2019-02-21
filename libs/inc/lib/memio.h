131e1e7c47239d2945f86f1e1f53ac6cfc373e07
Hidekazu Kato <hkato.sssl@gmail.com>
2019-02-21 20:37:20 +0900

    adding the new library.
    	new file:   libs/inc/lib/memio.h

---
+++ libs/inc/lib/memio.h
@@ -0,0 +1,49 @@
+/*
+ * lib/memio.h
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#ifndef LIB_MEMIO_H
+#define LIB_MEMIO_H
+
+/* includes */
+
+/* defines */
+
+#ifndef ASSEMBLY
+
+/* includes */
+
+#include <stdint.h>
+
+/* defines */
+
+/* types */
+
+/* variables */
+
+/* functions */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define REG_WRITE64(b, r, d)   (*(volatile uint64_t *)((b) + (r)) = (uint64_t)(d))
+#define REG_WRITE32(b, r, d)   (*(volatile uint32_t *)((b) + (r)) = (uint32_t)(d))
+#define REG_WRITE16(b, r, d)   (*(volatile uint16_t *)((b) + (r)) = (uint16_t)(d))
+#define REG_WRITE8(b, r, d)    (*(volatile uint8_t *)((b) + (r)) = (uint8_t)(d))
+
+#define REG_READ64(b, r)       (*(volatile uint64_t *)((b) + (r)))
+#define REG_READ32(b, r)       (*(volatile uint32_t *)((b) + (r)))
+#define REG_READ16(b, r)       (*(volatile uint16_t *)((b) + (r)))
+#define REG_READ8(b, r)        (*(volatile uint8_t *)((b) + (r)))
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASSEMBLY */
+
+#endif /* LIB_MEMIO_H */
+

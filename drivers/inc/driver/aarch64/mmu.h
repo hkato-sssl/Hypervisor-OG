f45ad2d9070d5afebb67c15486a859f20c689b2e
Hidekazu Kato <hkato.sssl@gmail.com>
2019-03-05 23:16:11 +0900

    マクロ再定義/初期化パラメータ追加
    ・WT属性に関する定義を削除。（最近のプロセッサでは無効となっている場合が見受けられる為）
    ・コード整理
    
    	modified:   drivers/inc/driver/aarch64/mmu.h
    	modified:   drivers/src/aarch64/mmu/aarch64_mmu_init.c

--- drivers/inc/driver/aarch64/mmu.h
+++ drivers/inc/driver/aarch64/mmu.h
@@ -22,7 +22,20 @@
 
 /* defines */
 
+#ifndef ASSEMBLY
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* includes */
+
+#include <stdint.h>
+
+/* defines */
+
 #define NR_MAIR_ATTRS               8           /* number of attribute in MAIR_ELx */
+#define	MMU_MAIR_ATTR(n, v)			((uint64_t)(v) << ((n) * 8))
 
 /* Device memory type */
 #define MEM_ATTR_DEVICE_nGnRnE      0x00
@@ -46,72 +59,59 @@
 #define MEM_ATTR_NORMAL_OWTRAWA     0xb0        /* Write-Through, Read-Allocate, Write-Allocate */
 #define MEM_ATTR_NORMAL_OWBWA       0xd0        /* Write-Back, Wite-Allocate */
 #define MEM_ATTR_NORMAL_OWBRA       0xe0        /* Write-Back, Read-Allocate */
+#define MEM_ATTR_NORMAL_OWBRAWA     0xf0        /* Write-Back, Read-Allocate, Wite-Allocate */
 
 #define MEM_ATTR_NORMAL_NC          (MEM_ATTR_NORMAL_ONC | MEM_ATTR_NORMAL_INC)
-#define MEM_ATTR_NORMAL_WT          (MEM_ATTR_NORMAL_OWTRAWA | MEM_ATTR_NORMAL_IWTRAWA)
-#define MEM_ATTR_NORMAL_WB          (MEM_ATTR_NORMAL_OWBRAWA | MEM_ATTR_NORMAL_IWBRAWA)
-
-#define MMU_MAIR_ATTR(n, v)         ((uint64_t)(v) << ((n) * 8))
+#define MEM_ATTR_NORMAL_WB          (MEM_ATTR_NORMAL_OWBRA | MEM_ATTR_NORMAL_IWBRA)
+#define MEM_ATTR_NORMAL_WBWA        (MEM_ATTR_NORMAL_OWBRAWA | MEM_ATTR_NORMAL_IWBRAWA)
 
 /* Next-level descriptor attriutes */
 
-#define MMU_DESC_NSTABLE        BIT(63)
-#define MMU_DESC_APTABLE(n)     (((n) & 3ULL) << 61)
-#define MMU_DESC_UXNTABLE       BIT(60)
-#define MMU_DESC_XNTABLE        BIT(60)
-#define MMU_DESC_PNTABLE        BIT(59)
+#define MMU_DESC_NSTABLE            BIT(63)
+#define MMU_DESC_APTABLE(n)         (((n) & 3ULL) << 61)
+#define MMU_DESC_UXNTABLE           BIT(60)
+#define MMU_DESC_XNTABLE            BIT(60)
+#define MMU_DESC_PNTABLE            BIT(59)
 
 /*
  * Attribute fields for VMSAv8-64 stage 1 Block and
  * Page descriptors
  */
 
-#define MMU_DESC_UXN            BIT(54)
-#define MMU_DESC_PBHA(n)        (((n) & 0x0fULL) << 59)
-#define MMU_DESC_XN             BIT(54)
-#define MMU_DESC_PXN            BIT(53)
-#define MMU_DESC_CONTIGUOUS     BIT(52)
-#define MMU_DESC_DBM            BIT(51)
+#define MMU_DESC_UXN                BIT(54)
+#define MMU_DESC_PBHA(n)            (((n) & 0x0fULL) << 59)
+#define MMU_DESC_XN                 BIT(54)
+#define MMU_DESC_PXN                BIT(53)
+#define MMU_DESC_CONTIGUOUS         BIT(52)
+#define MMU_DESC_DBM                BIT(51)
 
-#define MMU_DESC_NT             BIT(16)
-#define MMU_DESC_OA(n)          (((n) & 0x0fULL) << 12)
-#define MMU_DESC_NG             BIT(11)
-#define MMU_DESC_AF             BIT(10)
-#define MMU_DESC_SH(n)          (((n) & 3ULL) << 8)
-#define MMU_DESC_AP(n)          (((n) & 3ULL) << 6)
-#define MMU_DESC_ATTRINDX(n)    (((n) & 7ULL) << 2)
+#define MMU_DESC_NT                 BIT(16)
+#define MMU_DESC_OA(n)              (((n) & 0x0fULL) << 12)
+#define MMU_DESC_NG                 BIT(11)
+#define MMU_DESC_AF                 BIT(10)
+#define MMU_DESC_SH(n)              (((n) & 3ULL) << 8)
+#define MMU_DESC_AP(n)              (((n) & 3ULL) << 6)
+#define MMU_DESC_ATTRINDX(n)        (((n) & 7ULL) << 2)
 
-#ifndef ASSEMBLY
+#define MMU_ATTR_AP_RW              0   /* Read-Wirte, EL0 isn't accessible */
+#define MMU_ATTR_AP_RW_EL0          1   /* Read-Write, EL0 is accessible */
+#define MMU_ATTR_AP_RO              2   /* Read-Only, EL0 isn't accessible */
+#define MMU_ATTR_AP_RO_EL0          3   /* Read-Only, EL0 is accessible */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#define MMU_ATTR_SH_NSH             0   /* Non-shareable */
+#define MMU_ATTR_SH_OSH             2   /* Outer Shareable */
+#define MMU_ATTR_SH_ISH             3   /* Inner Shareable */
 
-/* includes */
+#define MMU_TCR_SH_NSH              0   /* Non-shareable */
+#define MMU_TCR_SH_OSH              2   /* Outer Shareable */
+#define MMU_TCR_SH_ISH              3   /* Inner Shareable */
 
-#include <stdint.h>
-
-/* defines */
+#define MMU_TCR_RGN_NC              0   /* Normal memory, Non-cacheable */
+#define MMU_TCR_RGN_WBWA            1   /* Normal memory, Write-Back Write-Allocate Cacheable */
+#define MMU_TCR_RGN_WT              2   /* Normal memory, Write-Through Cacheable */
+#define MMU_TCR_RGN_WB              3   /* Normal memory, Write-Back no Write-Allocate Cacheable */
 
 /* types */
-
-#define MMU_ATTR_AP_RW          0   /* Read-Wirte, EL0 isn't accessible */
-#define MMU_ATTR_AP_RW_EL0      1   /* Read-Write, EL0 is accessible */
-#define MMU_ATTR_AP_RO          2   /* Read-Only, EL0 isn't accessible */
-#define MMU_ATTR_AP_RO_EL0      3   /* Read-Only, EL0 is accessible */
-
-#define MMU_ATTR_SH_NSH         0   /* Non-shareable */
-#define MMU_ATTR_SH_OSH         2   /* Outer Shareable */
-#define MMU_ATTR_SH_ISH         3   /* Inner Shareable */
-
-#define MMU_TCR_SH_NSH          0   /* Non-shareable */
-#define MMU_TCR_SH_OSH          2   /* Outer Shareable */
-#define MMU_TCR_SH_ISH          3   /* Inner Shareable */
-
-#define MMU_TCR_RGN_NC          0   /* Normal memory, Non-cacheable */
-#define MMU_TCR_RGN_WBWA        1   /* Normal memory, Write-Back Write-Allocate Cacheable */
-#define MMU_TCR_RGN_WT          2   /* Normal memory, Write-Through Cacheable */
-#define MMU_TCR_RGN_WB          3   /* Normal memory, Write-Back no Write-Allocate Cacheable */
 
 struct aarch64_mmu_attr {
     /* upper attributes */
@@ -154,23 +154,23 @@ struct aarch64_mmu_block_pool_configure {
 };
 
 struct aarch64_mmu_tcr {
-    uint32_t        sz:6;
-    uint32_t        sh:2;
-    uint32_t        irgn:2;
-    uint32_t        orgn:2;
+    uint8_t     sz:6;
+    uint8_t     sh:2;
+    uint8_t     irgn:2;
+    uint8_t     orgn:2;
 };
 
 struct aarch64_mmu_trans_table {
     uint64_t        *addr;
     uint16_t        asid;
-    uint64_t        mair;
+    uint64_t		mair;
     struct aarch64_mmu_tcr tcr;
     struct aarch64_mmu_block_pool pool;
 };
 
 struct aarch64_mmu_trans_table_configure {
     uint16_t        asid;
-    uint64_t        mair;
+    uint64_t		mair;
     struct aarch64_mmu_tcr tcr;
     struct aarch64_mmu_block_pool_configure pool;
 };
--- drivers/src/aarch64/mmu/aarch64_mmu_init.c
+++ drivers/src/aarch64/mmu/aarch64_mmu_init.c
@@ -21,11 +21,11 @@
 
 /* functions */
 
-static errno_t validate_parameters(struct aarch64_mmu_trans_table *tt, struct aarch64_mmu_trans_table_configure const *config)
+static errno_t validate_parameters(struct aarch64_mmu_trans_table *tt, struct aarch64_mmu_trans_table_configure const *conf)
 {
     errno_t ret;
 
-    if ((tt != NULL) && (config != NULL) && (config->pool.block_sz == MMU_BLOCK_SZ)) {
+    if ((tt != NULL) && (conf != NULL) && (conf->pool.block_sz == MMU_BLOCK_SZ)) {
         ret = SUCCESS;
     } else {
         ret = -EINVAL;
@@ -34,27 +34,33 @@ static errno_t validate_parameters(struct aarch64_mmu_trans_table *tt, struct aa
     return ret;
 }
 
-static errno_t init_trans_table(struct aarch64_mmu_trans_table *tt, struct aarch64_mmu_trans_table_configure const *config)
+static errno_t init_trans_table(struct aarch64_mmu_trans_table *tt, struct aarch64_mmu_trans_table_configure const *conf)
 {
-    tt->asid = config->asid;
-    tt->mair = config->mair;
-    tt->tcr = config->tcr;
+    tt->asid = conf->asid;
     aarch64_mmu_memclr(tt->addr, MMU_BLOCK_SZ);
+
+    /*
+     * 現状ではTCR関連のパラメータは固定値とする
+     */
+    tt->tcr.sz = 16;    /* The region size is 2^48 bytes. */
+    tt->tcr.sh = MMU_TCR_SH_ISH;
+    tt->tcr.irgn = MMU_TCR_RGN_WBWA;
+    tt->tcr.orgn = MMU_TCR_RGN_WBWA;
 
     return SUCCESS;
 }
 
-static errno_t mmu_init(struct aarch64_mmu_trans_table *tt, struct aarch64_mmu_trans_table_configure const *config)
+static errno_t mmu_init(struct aarch64_mmu_trans_table *tt, struct aarch64_mmu_trans_table_configure const *conf)
 {
     errno_t ret;
 
     memset(tt, 0, sizeof(*tt));
 
-    ret = aarch64_mmu_block_pool_init(&(tt->pool), &(config->pool));
+    ret = aarch64_mmu_block_pool_init(&(tt->pool), &(conf->pool));
     if (ret == SUCCESS) {
         tt->addr = aarch64_mmu_block_calloc(&(tt->pool), MMU_BLOCK_SZ);
         if (tt->addr != NULL) {
-            ret = init_trans_table(tt, config);
+            ret = init_trans_table(tt, conf);
         } else {
             ret = -ENOMEM;
         }
@@ -63,13 +69,13 @@ static errno_t mmu_init(struct aarch64_mmu_trans_table *tt, struct aarch64_mmu_t
     return ret;
 }
 
-errno_t aarch64_mmu_init(struct aarch64_mmu_trans_table *tt, struct aarch64_mmu_trans_table_configure const *config)
+errno_t aarch64_mmu_init(struct aarch64_mmu_trans_table *tt, struct aarch64_mmu_trans_table_configure const *conf)
 {
     errno_t ret;
 
-    ret = validate_parameters(tt, config);
+    ret = validate_parameters(tt, conf);
     if (ret == SUCCESS) {
-        ret = mmu_init(tt, config);
+        ret = mmu_init(tt, conf);
     }
 
     return ret;

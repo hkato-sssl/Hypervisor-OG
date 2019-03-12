92f252d0a994f3b146df1301f091e6015fb0e441
Hidekazu Kato <hkato.sssl@gmail.com>
2019-03-12 19:53:31 +0900

    GIC400レジスタ定義追加
    	modified:   drivers/inc/driver/xilinx/mpsoc.h

--- drivers/inc/driver/xilinx/mpsoc.h
+++ drivers/inc/driver/xilinx/mpsoc.h
@@ -11,8 +11,10 @@
 
 /* defines */
 
-#define REG_UART0       0xff000000
-#define REG_UART1       0xff010000
+#define REG_UART0           0xff000000
+#define REG_UART1           0xff010000
+#define	REG_GIC400_CPUIF    0xf9020000
+#define	REG_GIC400_DIST     0xf9010000
 
 #endif /* DRIVER_XILINX_MPSOC_H */
 

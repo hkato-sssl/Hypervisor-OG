94c063716bbba6991db5a4dfe5340a9c3a20fca2
Hidekazu Kato <hkato.sssl@gmail.com>
2019-10-17 22:59:28 +0900

    memset()追加。
    一時的な処置。後で実装内容を最適化する。
    
    	new file:   zcu104/src/memset.c

---
+++ zcu104/src/memset.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include <string.h>
+
+eoid *memset(void *s, int c, size_t n)
+{
+    char *p;
+
+    p = s;
+    for (; n > 0; --n) {
+        *p++ = (char)c;
+    }
+
+    return s;
+}

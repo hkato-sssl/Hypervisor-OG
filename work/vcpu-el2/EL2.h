882806e903756783694868fac3ec98675e113dd1
Hidekazu Kato <hkato.sssl@gmail.com>
2019-10-24 15:42:24 +0900

    EL2 system register定義生成環境
    	new file:   work/vcpu-el2/EL2.csv
    	new file:   work/vcpu-el2/EL2.h
    	new file:   work/vcpu-el2/readme.md
    	new file:   work/vcpu-el2/vcpu-el2.py

---
+++ work/vcpu-el2/EL2.h
@@ -0,0 +1,11 @@
+#define VCPU_VPIDR_EL2                VCPU_SYS_EL2_BASE        // Virtualization Processor ID Register
+#define VCPU_VMPIDR_EL2               (VCPU_SYS_EL2_BASE + 1)  // Virtualization Multiprocessor ID Register
+#define VCPU_IFSR32_EL2               (VCPU_SYS_EL2_BASE + 2)  // Instruction Fault Status Register
+#define VCPU_HPFAR_EL2                (VCPU_SYS_EL2_BASE + 3)  // Hypervisor IPA Fault Address Register
+#define VCPU_VTTBR_EL2                (VCPU_SYS_EL2_BASE + 4)  // Virtualization Translation Table Base Address Register
+#define VCPU_VTCR_EL2                 (VCPU_SYS_EL2_BASE + 5)  // Virtualization Translation Control Register
+#define VCPU_HCR_EL2                  (VCPU_SYS_EL2_BASE + 6)  // Hypervisor Configuration Register
+#define VCPU_HSTR_EL2                 (VCPU_SYS_EL2_BASE + 7)  // Hyp System Trap Register
+#define VCPU_HACR_EL2                 (VCPU_SYS_EL2_BASE + 8)  // Hyp Auxiliary Configuration Register
+#define VCPU_DACR32_EL2               (VCPU_SYS_EL2_BASE + 9)  // Domain Access Control Register
+#define VCPU_CNTVOFF_EL2              (VCPU_SYS_EL2_BASE + 10) // Counter-timer Virtual Offset register
---
+++ work/vcpu-el2/vcpu-el2.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+#
+# (C) 2019 Hidekazu Kato
+#
+"""Overview:
+    EL2.csvからvcpu用マクロ定義を生成
+    A53フィールドが〇のレジスタを生成対象とする
+
+Usage:
+    vcpu-el2.py [-h] <file>
+
+Options:
+    -h       : help
+    <file>   : csv file name.
+"""
+from docopt import docopt
+import os
+import re
+import sys
+import string
+
+base_name = 'VCPU_SYS_EL2_BASE';
+
+def readlines(file_name):
+    try:
+        f = open(file_name, "r")
+        lines = f.readlines()
+    except IOError as e:
+        sys.exit(e)
+    finally:
+        f.close()
+
+    return lines
+
+def translate(lines):
+    global base_name;
+    offset = 0;
+    for ln in lines:
+        fields = ln.split(',')
+        if fields[4] == '〇':
+            def_name = '#define VCPU_' + fields[0] + ' ' * (24 - len(fields[0]))
+            if offset == 0:
+                def_val = base_name
+            else:
+                def_val = '(' + base_name + " + " + str(offset) + ')'
+            comment = (' ' * (24 - len(def_val))) + '// ' + fields[3]
+            print(def_name, def_val, comment)
+            offset += 1
+
+def main():
+    args = docopt(__doc__)
+    file_name = args["<file>"]
+    lines = readlines(file_name)
+
+    # remove the first element
+    del lines[0]
+
+    translate(lines)
+    return 0
+
+if __name__ == "__main__":
+    main()

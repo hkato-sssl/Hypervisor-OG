a17421cc9be76147723dc7568b494a55254661e4
Hidekazu Kato <hkato.sssl@gmail.com>
2019-10-26 19:44:41 +0900

    adding the test code of Generic Timer.
    	new file:   drivers/inc/driver/aarch64/test/gt.h
    	new file:   drivers/src/aarch64/test/gt/0.c

---
+++ drivers/inc/driver/aarch64/test/gt.h
@@ -0,0 +1,39 @@
+/*
+ * driver/aarch64/test/gt.h - test for Generic Timer
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#ifndef DRIVER_AARCH64_TEST_GT_H
+#define DRIVER_AARCH64_TEST_GT_H
+
+/* includes */
+
+/* defines */
+
+#ifndef ASSEMBLY
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* includes */
+
+/* defines */
+
+/* types */
+
+/* variables */
+
+/* functions */
+
+void test_gt_0(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASSEMBLY */
+
+#endif /* DRIVER_AARCH64_TEST_GT_H */
+
---
+++ drivers/src/aarch64/test/gt/0.c
@@ -0,0 +1,44 @@
+/*
+ * aarch64/test/gt/0.c -- test of Generic Timer
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#include <stdint.h>
+#include "lib/system/printk.h"
+#include "driver/aarch64.h"
+#include "driver/aarch64/system_register.h"
+#include "driver/aarch64/test/gt.h"
+
+/* defines */
+
+/* types */
+
+/* prototypes */
+
+/* variables */
+
+/* functions */
+
+void test_gt_0(void)
+{
+    int i;
+    uintptr_t d0;
+    uintptr_t d;
+
+    printk("<%s>\n", __func__);
+
+    d = aarch64_read_cntfrq_el0();
+    printk("CNTFRQ_EL0: %llu\n", d);    
+
+    aarch64_isb();
+    d0 = aarch64_read_cntpct_el0();
+    printk("CNTPCT_EL0: %llu\n", d0);   
+
+    for (i = 0; i < 10; ++i) {
+        aarch64_isb();
+        d = aarch64_read_cntpct_el0();
+        printk("#%d: %llu\n", i, d - d0);
+        d0 = d;
+    }
+}

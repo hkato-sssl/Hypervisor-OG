14fbfc31831c83d8c53b0d8a1c08898ff7d5948b
Hidekazu Kato <hkato.sssl@gmail.com>
2019-10-27 00:57:40 +0900

    adding the generated file.
    	new file:   work/vpc-regs/vpc_regs.h

---
+++ work/vpc-regs/vpc_regs.h
@@ -0,0 +1,109 @@
+#define VPC_X0                  0
+#define VPC_X1                  1
+#define VPC_X2                  2
+#define VPC_X3                  3
+#define VPC_X4                  4
+#define VPC_X5                  5
+#define VPC_X6                  6
+#define VPC_X7                  7
+#define VPC_X8                  8
+#define VPC_X9                  9
+#define VPC_X10                 10
+#define VPC_X11                 11
+#define VPC_X12                 12
+#define VPC_X13                 13
+#define VPC_X14                 14
+#define VPC_X15                 15
+#define VPC_X16                 16
+#define VPC_X17                 17
+#define VPC_X18                 18
+#define VPC_X19                 19
+#define VPC_X20                 20
+#define VPC_X21                 21
+#define VPC_X22                 22
+#define VPC_X23                 23
+#define VPC_X24                 24
+#define VPC_X25                 25
+#define VPC_X26                 26
+#define VPC_X27                 27
+#define VPC_X28                 28
+#define VPC_X29                 29
+#define VPC_X30                 30
+#define VPC_SP_EL0              31
+#define VPC_SP_EL1              32
+#define VPC_PC                  33
+#define VPC_ELR_EL1             34
+#define VPC_SPSR_EL1            35
+#define VPC_SPSR_ABT            36
+#define VPC_SPSR_IRQ            37
+#define VPC_SPSR_FIQ            38
+#define VPC_SPSR_UND            39
+#define VPC_CSSELR_EL1          40      // Cache Size Selection Register
+#define VPC_ESR_EL1             41      // Exception Syndrome Register
+#define VPC_FAR_EL1             42      // Fault Address Register
+#define VPC_VBAR_EL1            43      // Vector Base Address Register
+#define VPC_SCTLR_EL1           44      // System Control Register
+#define VPC_TTBR0_EL1           45      // Translation Table Base Register 0
+#define VPC_TTBR1_EL1           46      // Translation Table Base Register 1
+#define VPC_TCR_EL1             47      // Translation Control Register
+#define VPC_MAIR_EL1            48      // Memory Attribute Indirection Register
+#define VPC_CONTEXTIDR_EL1      49      // Context ID Register
+#define VPC_CPACR_EL1           50      // Architectural Feature Access Control Register
+#define VPC_PAR_EL1             51      // Physical Address Register
+#define VPC_TPIDR_EL0           52      // Thread Pointer/ID Register
+#define VPC_TPIDR_EL1           53      // Thread Pointer/ID Register
+#define VPC_TPIDRRO_EL0         54      // Thread Pointer/ID Register
+#define VPC_CNTKCTL_EL1         55      // Counter-timer Kernel Control register
+#define VPC_CNTP_TVAL_EL0       56      // Counter-timer Physical Timer TimerValue register
+#define VPC_CNTP_CTL_EL0        57      // Counter-timer Physical Timer Control register
+#define VPC_CNTP_CVAL_EL0       58      // Counter-timer Physical Timer CompareValue register
+#define VPC_CNTV_TVAL_EL0       59      // Counter-timer Virtual Timer TimerValue register
+#define VPC_CNTV_CTL_EL0        60      // Counter-timer Virtual Timer Control register
+#define VPC_CNTV_CVAL_EL0       61      // Counter-timer Virtual Timer CompareValue register
+#define VPC_VPIDR_EL2           62      // Virtualization Processor ID Register
+#define VPC_VMPIDR_EL2          63      // Virtualization Multiprocessor ID Register
+#define VPC_IFSR32_EL2          64      // Instruction Fault Status Register
+#define VPC_HPFAR_EL2           65      // Hypervisor IPA Fault Address Register
+#define VPC_VTTBR_EL2           66      // Virtualization Translation Table Base Address Register
+#define VPC_VTCR_EL2            67      // Virtualization Translation Control Register
+#define VPC_HCR_EL2             68      // Hypervisor Configuration Register
+#define VPC_HSTR_EL2            69      // Hyp System Trap Register
+#define VPC_HACR_EL2            70      // Hyp Auxiliary Configuration Register
+#define VPC_DACR32_EL2          71      // Domain Access Control Register
+#define VPC_CNTVOFF_EL2         72      // Counter-timer Virtual Offset register
+#define VPC_FPCR                73      // Floating-point Control Register 
+#define VPC_FPSR                74      // Floating-point Status Register 
+#define VPC_FPEXC32_EL2         75      // Floating-point Exception Control Register
+#define VPC_Q0                  76
+#define VPC_Q1                  78
+#define VPC_Q2                  80
+#define VPC_Q3                  82
+#define VPC_Q4                  84
+#define VPC_Q5                  86
+#define VPC_Q6                  88
+#define VPC_Q7                  90
+#define VPC_Q8                  92
+#define VPC_Q9                  94
+#define VPC_Q10                 96
+#define VPC_Q11                 98
+#define VPC_Q12                 100
+#define VPC_Q13                 102
+#define VPC_Q14                 104
+#define VPC_Q15                 106
+#define VPC_Q16                 108
+#define VPC_Q17                 110
+#define VPC_Q18                 112
+#define VPC_Q19                 114
+#define VPC_Q20                 116
+#define VPC_Q21                 118
+#define VPC_Q22                 120
+#define VPC_Q23                 122
+#define VPC_Q24                 124
+#define VPC_Q25                 126
+#define VPC_Q26                 128
+#define VPC_Q27                 130
+#define VPC_Q28                 132
+#define VPC_Q29                 134
+#define VPC_Q30                 136
+#define VPC_Q31                 138
+#define NR_VCPU_REGS            140

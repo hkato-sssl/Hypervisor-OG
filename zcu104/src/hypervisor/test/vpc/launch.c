035e2ab602b24daad8c0d79c61754ed3e8eb2b57
Hidekazu Kato <hkato.sssl@gmail.com>
2019-10-30 23:17:16 +0900

    adding the test code for Hypervisor.
    	new file:   src/hypervisor/test/vpc/launch.c
    	new file:   src/hypervisor/test/vpc/launch_asm.S

---
+++ zcu104/src/hypervisor/test/vpc/launch.c
@@ -0,0 +1,50 @@
+/*
+ * test/vpc/launch.c
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include "lib/system/errno.h"
+#include "lib/system/printk.h"
+#include "hypervisor/vpc.h"
+
+/* defines */
+
+/* types */
+
+/* prototypes */
+
+void test_vpc_launch_entry(void);
+
+/* variables */
+
+static struct vpc vpc;
+static struct vpc_config config;
+static uint64_t register_array[NR_VPC_REGS];
+
+/* functions */
+
+void test_vpc_launch(void)
+{
+    errno_t ret;
+
+    printk("<%s>\n", __func__);
+
+    memset(&config, 0, sizeof(config));
+
+    config.owner = (void*)0xff; // dummy
+    config.regs = register_array;
+    config.aarch = 64;
+    config.gpr.pc = (uint64_t)test_vpc_launch_entry;
+    config.gpr.sp = 0;
+
+    ret = vpc_configure(&vpc, &config);
+    printk("vpc_configure() -> %d\n", ret);
+
+    if (ret == SUCCESS) {
+        vpc_launch(&vpc);
+    }
+}
+

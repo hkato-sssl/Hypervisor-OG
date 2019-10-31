7a10589ffab01acdc36f4e07c9f5b215274662e0
Hidekazu Kato <hkato.sssl@gmail.com>
2019-10-31 22:15:28 +0900

    HCR_EL2 ビット定義追加
    	modified:   drivers/inc/driver/aarch64/system_register.h

--- drivers/inc/driver/aarch64/system_register.h
+++ drivers/inc/driver/aarch64/system_register.h
@@ -84,6 +84,44 @@
 #define CPTR_TFP            BIT(10)
 #define CPTR_RES1_EL2       BITS(13, 12)
 
+/* HCR_EL2 */
+#define HCR_MIOCNCE         BIT(38)
+#define HCR_ID              BIT(33)
+#define HCR_CD              BIT(32)
+#define HCR_RW              BIT(31)
+#define HCR_TRVM            BIT(30)
+#define HCR_HCD             BIT(29)
+#define HCR_TDZ             BIT(28)
+#define HCR_TGE             BIT(27)
+#define HCR_TVM             BIT(26)
+#define HCR_TTLB            BIT(25)
+#define HCR_TPU             BIT(24)
+#define HCR_TPC             BIT(23)
+#define HCR_TSW             BIT(22)
+#define HCR_TACR            BIT(21)
+#define HCR_TIDCP           BIT(20)
+#define HCR_TSC             BIT(19)
+#define HCR_TID3            BIT(18)
+#define HCR_TID2            BIT(17)
+#define HCR_TID1            BIT(16)
+#define HCR_TID0            BIT(15)
+#define HCR_TWE             BIT(14)
+#define HCR_TWI             BIT(13)
+#define HCR_DC              BIT(12)
+#define HCR_BSU_MASK        BITS(11, 10)
+#define HCR_BSU_MSB         11
+#define HCR_BSU_LSB         10
+#define HCR_FB              BIT(9)
+#define HCR_VSE             BIT(8)
+#define HCR_VI              BIT(7)
+#define HCR_VF              BIT(6)
+#define HCR_AMO             BIT(5)
+#define HCR_IMO             BIT(4)
+#define HCR_FMO             BIT(3)
+#define HCR_PTW             BIT(2)
+#define HCR_SWIO            BIT(1)
+#define HCR_VM              BIT(0)
+
 #ifndef ASSEMBLY
 
 /* includes */

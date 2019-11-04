8b40e64290e322f9d48a82ce051ecfc4256f9819
Hidekazu Kato <hkato.sssl@gmail.com>
2019-11-04 17:18:43 +0900

    testフォルダ配置変更
    	renamed:    drivers/inc/driver/aarch64/test/gt.h -> drivers/inc/driver/test/aarch64/gt.h
    	new file:   drivers/inc/driver/test/aarch64/stage2.h
    	renamed:    drivers/src/aarch64/test/gt/0.c -> drivers/src/test/aarch64/gt/0.c
    	renamed:    drivers/src/aarch64/test/gt/cntp_0.c -> drivers/src/test/aarch64/gt/cntp_0.c
    	new file:   drivers/src/test/aarch64/stage2/01.c

---
+++ drivers/inc/driver/test/aarch64/stage2.h
@@ -0,0 +1,37 @@
+/*
+ * driver/test/aarch64/stage2.h
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#ifndef DRIVER_TEST_AARCH64_STAGE2_H
+#define DRIVER_TEST_AARCH64_STAGE2_H
+
+/* includes */
+
+/* defines */
+
+#ifndef ASSEMBLY
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* includes */
+
+/* defines */
+
+/* types */
+
+/* variables */
+
+/* functions */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASSEMBLY */
+
+#endif /* DRIVER_TEST_AARCH64_STAGE2_H */
+
---
+++ drivers/src/test/aarch64/stage2/01.c
@@ -0,0 +1,27 @@
+/*
+ * test/stage2/01.c
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#include <stdint.h>
+#include "driver/aarch64/mmu.h"
+
+/* defines */
+
+/* types */
+
+/* prototypes */
+
+void test_stage2_01_entry(void);
+
+/* variables */
+
+static 
+
+/* functions */
+
+void test_stage2_01(void)
+{
+
+}

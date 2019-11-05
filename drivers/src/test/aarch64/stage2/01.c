550abccb700990dc38cdf1c421a75b2632ba43cf
Hidekazu Kato <hkato.sssl@gmail.com>
2019-11-05 14:25:06 +0900

    implementing test codes.
    	modified:   drivers/src/test/aarch64/stage2/01.c

--- drivers/src/test/aarch64/stage2/01.c
+++ drivers/src/test/aarch64/stage2/01.c
@@ -5,6 +5,8 @@
  */
 
 #include <stdint.h>
+#include "lib/system/errno.h"
+#include "lib/system/printk.h"
 #include "driver/aarch64/mmu.h"
 
 /* defines */
@@ -17,11 +19,40 @@ void test_stage2_01_entry(void);
 
 /* variables */
 
-static 
+static char mem_block_region[32][4096] __attribute__((aligned(4096)));
+static struct aarch64_mmu_trans_table st2;
 
 /* functions */
 
+void init(void)
+{
+    errno_t ret;
+    struct aarch64_mmu_trans_table_configuration config;
+
+    config.stage = MMU_STAGE1;
+    config.stage2.vmid = 0;
+    //config.tcr.
+    config.pool.block_sz = 4096;
+    config.pool.block_region.addr = mem_block_region;
+    config.pool.block_region.size = sizeof(mem_block_region);
+
+    ret = aarch64_mmu_init(&st2, &config);
+    printk("aarch64_mmu_init() -> %d\n", ret);
+}
+
 void test_stage2_01(void)
 {
+    struct aarch64_mmu_attr attr;
+
+    printk("<%s>\n", __func__);
+    init();
+
+    attr.stage = MMU_STAGE2;
+    attr.stage2.xn = 0;
+    attr.stage2.sh = MMU_ATTR_ISH;
+    attr.stage2.s2ap = MMU_ATTR_S2AP = RW;
+    attr.stage2.memattr = 0;
+
 
 }
+

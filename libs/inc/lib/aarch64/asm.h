af65ddc8391cb4c44253a4e3a47a4868e8662535
Hidekazu Kato <hkato.sssl@gmail.com>
2019-11-07 17:53:14 +0900

    lib/aarch64.hを削除
    アセンブラ用マクロ定義はlib/aarch64/asm.hに移動
    
    	modified:   drivers/src/aarch64/mmu/aarch64_mmu_tlbi_all.S
    	modified:   drivers/src/aarch64/mmu/aarch64_mmu_tlbi_va.S
    	modified:   drivers/src/aarch64/system_register.S
    	deleted:    libs/inc/lib/aarch64.h
    	modified:   libs/inc/lib/aarch64/asm.h

--- libs/inc/lib/aarch64/asm.h
+++ libs/inc/lib/aarch64/asm.h
@@ -12,12 +12,23 @@
 /* defines */
 
 #ifdef ASSEMBLY
+        .macro      BR_EL, table
+        adr         ip0, \table
+        mrs         ip1, CurrentEL
+        add         ip0, ip0, ip1, lsl #1
+        ldr         ip1, [ip0]
+        br          ip1
+        .balign      8
+        .endm
 
         .macro mov32, reg, val
         movz \reg, #:abs_g0_nc:\val
         movk \reg, #:abs_g1:\val
         .endm
 
+        .macro adrl, reg, val
+        ldr \reg, =\val
+        .endm
 #endif /* ASSEMBLY */
 
 #endif /* LIB_AARCH64_ASM_H */

9044cc7a5045ee66e6402fa82379e5900a520b8c
Hidekazu Kato <hkato.sssl@gmail.com>
2019-11-08 16:52:10 +0900

    adding new macros.
    	modified:   drivers/inc/driver/aarch64/system_register/mair.h

--- drivers/inc/driver/aarch64/system_register/mair.h
+++ drivers/inc/driver/aarch64/system_register/mair.h
@@ -11,7 +11,67 @@
 
 /* defines */
 
-#define NR_MAIR_ATTRS   8
+#define NR_MAIR_ATTRS       8
+
+#define MAIR_ATTR(n, v)     (((v) & 0xff) << (n))
+
+/* Device memory */
+#define MAIR_ATTR_DEVICE_nGnRnE     0x00
+#define MAIR_ATTR_DEVICE_nGnRE      0x04
+#define MAIR_ATTR_DEVICE_nGRE       0x08
+#define MAIR_ATTR_DEVICE_GRE        0x0c
+
+/* Normal Memory, Outer Write-through transient */
+#define MAIR_ATTR_NORMAL_OWTTWA     0x10
+#define MAIR_ATTR_NORMAL_OWTTRA     0x20
+#define MAIR_ATTR_NORMAL_OWTTRAWA   0x30
+
+/* Normal Memory, Outer Non-cacheable */
+#define MAIR_ATTR_NORMAL_ONC        0x40
+
+/* Normal Memory, Outer Write-back transient */
+#define MAIR_ATTR_NORMAL_OWBTWA     0x50
+#define MAIR_ATTR_NORMAL_OWBTRA     0x60
+#define MAIR_ATTR_NORMAL_OWBTRAWA   0x70
+
+/* Normal Memory, Outer Write-through non-transient */
+#define MAIR_ATTR_NORMAL_OWTWA      0x90
+#define MAIR_ATTR_NORMAL_OWTRA      0xa0
+#define MAIR_ATTR_NORMAL_OWTRAWA    0xb0
+
+/* Normal Memory, Outer Write-back non-transient */
+#define MAIR_ATTR_NORMAL_OWBWA      0xd0
+#define MAIR_ATTR_NORMAL_OWBRA      0xe0
+#define MAIR_ATTR_NORMAL_OWBRAWA    0xf0
+
+/* Normal Memory, Inner Write-through transient */
+#define MAIR_ATTR_NORMAL_IWTTWA     0x01
+#define MAIR_ATTR_NORMAL_IWTTRA     0x02
+#define MAIR_ATTR_NORMAL_IWTTRAWA   0x03
+
+/* Normal Memory, Inner Non-cacheable */
+#define MAIR_ATTR_NORMAL_INC        0x04
+
+/* Normal Memory, Inner Write-back transient */
+#define MAIR_ATTR_NORMAL_IWBTWA     0x05
+#define MAIR_ATTR_NORMAL_IWBTRA     0x06
+#define MAIR_ATTR_NORMAL_IWBTRAWA   0x07
+
+/* Normal Memory, Inner Write-through non-transient */
+#define MAIR_ATTR_NORMAL_IWTWA      0x09
+#define MAIR_ATTR_NORMAL_IWTRA      0x0a
+#define MAIR_ATTR_NORMAL_IWTRAWA    0x0b
+
+/* Normal Memory, Inner Write-back non-transient */
+#define MAIR_ATTR_NORMAL_IWBWA      0x0d
+#define MAIR_ATTR_NORMAL_IWBRA      0x0e
+#define MAIR_ATTR_NORMAL_IWBRAWA    0x0f
+
+#define MAIR_ATTR_NORMAL_NC         (MAIR_ATTR_NORMAL_ONC | MAIR_ATTR_NORMAL_INC)
+#define MAIR_ATTR_NORMAL_WT         (MAIR_ATTR_NORMAL_OWTRA | MAIR_ATTR_NORMAL_IWTRA)
+#define MAIR_ATTR_NORMAL_WTWA       (MAIR_ATTR_NORMAL_OWTRAWA | MAIR_ATTR_NORMAL_IWTRAWA)
+#define MAIR_ATTR_NORMAL_WB         (MAIR_ATTR_NORMAL_OWBRA | MAIR_ATTR_NORMAL_IWBRA)
+#define MAIR_ATTR_NORMAL_WBWA       (MAIR_ATTR_NORMAL_OWBRAWA | MAIR_ATTR_NORMAL_IWBRAWA)
 
 #ifndef ASSEMBLY
 

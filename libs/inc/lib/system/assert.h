6574465c9a97a591090b19e73341ddead8cd0435
Hidekazu Kato <hkato.sssl@gmail.com>
2019-11-21 17:00:10 +0900

    例外処理用API追加   assert_exception()
    	modified:   drivers/src/aarch64/system_register.S
    	new file:   libs/inc/lib/system/assert.h
    	new file:   libs/src/system/assert_exception.S

---
+++ libs/inc/lib/system/assert.h
@@ -0,0 +1,39 @@
+/*
+ * system/assert.h
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#ifndef SYSTEM_ASSERT_H
+#define SYSTEM_ASSERT_H
+
+/* includes */
+
+/* defines */
+
+#ifndef ASSEMBLY
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* includes */
+
+/* defines */
+
+/* types */
+
+/* variables */
+
+/* functions */
+
+volatile void assert_exception(void *arg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASSEMBLY */
+
+#endif /* SYSTEM_ASSERT_H */
+

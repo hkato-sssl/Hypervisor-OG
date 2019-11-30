e821f3b0a0198f293b1d955516e9181f834cf2d8
Hidekazu Kato <hkato.sssl@gmail.com>
2019-11-30 23:30:50 +0900

    adding cache test code.
    	new file:   drivers/src/test/aarch64/cache/01_clean.c
    	new file:   drivers/src/test/aarch64/cache/01_clean_invd.c
    	new file:   drivers/src/test/aarch64/cache/01_invd.c
    	new file:   drivers/src/test/aarch64/cache/cache_test.h
    	new file:   drivers/src/test/aarch64/cache/common.c

---
+++ drivers/src/test/aarch64/cache/01_clean_invd.c
@@ -0,0 +1,131 @@
+/*
+ * test/aarch64/cache/01_clean_invd.c
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include "lib/bit.h"
+#include "lib/system/errno.h"
+#include "lib/system/printk.h"
+#include "lib/system/memio.h"
+#include "driver/aarch64/system_register.h"
+#include "driver/aarch64/system_register/tcr_elx.h"
+#include "driver/aarch64/system_register/mair_elx.h"
+#include "driver/aarch64/mmu.h"
+#include "driver/aarch64/cache.h"
+#include "cache_test.h"
+
+/* テスト項目：Clean-invalidate VA
+ *
+ * 1) 同じ物理メモリ領域をVA1/VA2の２つにマッピング
+ * 2) VA1/VA2共にWrite-back(Read-allocate)でマッピング
+ * 3) １度VA1/VA2の値を読み込む
+ * 4) VA1に書き込んだ値がVA2から参照可能である事を確認
+ * 5) PAでは値が変更されていない事を確認
+ * 6) VA1に対してclean-invalidateを実行
+ * 7) PAの値がVA1/VA2と同じ値になる事を確認
+ * 8) PA/VA1の順にに異なる値を書き込む
+ * 9) PAがVA1と同じ値である事を確認
+ *
+ * 前提条件：
+ * 1) MMU無効状態でテストコードを実行
+ * 2) Cacheの影響を排除する為にbss/data/stack領域全てをNon-cacheable
+ *    でマッピングする
+ * 3) EL2で実行
+ *    EL1はEL2/EL3による設定でキャッシュ保守操作自体の挙動が変更され
+ *    る為、MMU/Cache driverの動作確認には適さない。
+ */
+
+/* defines */
+
+/* test parameters */
+
+#define TEST_VA1                0xe0000000
+#define TEST_VA2                0xe0010000
+
+/* types */
+
+/* prototypes */
+
+/* variables */
+
+extern struct aarch64_mmu test_mmu;
+
+static char test_region[4096] __attribute__((aligned(4096)));
+
+/* functions */
+
+errno_t test_aarch64_cache_01_clean_invd(void)
+{
+    errno_t ret;
+    volatile char *va1;
+    volatile char *va2;
+    struct aarch64_mmu_attr attr;
+
+    printk("<%s>\n", __func__);
+
+    ret = test_aarch64_cache_init_memory_map();
+    printk("test_aarch64_cache_init_memory_map() -> %d\n", ret);
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    memset(&attr, 0, sizeof(attr));
+    attr.af = 1;
+    attr.sh = MMU_ATTR_SH_ISH;
+    attr.ap21 = MMU_ATTR_AP_RW;
+    attr.attrindx = MMU_ATTR_WB;
+
+    va1 = (void *)TEST_VA1;
+    va2 = (void *)TEST_VA2;
+
+    ret = aarch64_mmu_map(&test_mmu, (void *)va1, test_region, sizeof(test_region), &attr);
+    printk("VA1: aarch64_mmu_map() -> %d\n", ret);
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    ret = aarch64_mmu_map(&test_mmu, (void *)va2, test_region, sizeof(test_region), &attr);
+    printk("VA2: aarch64_mmu_map() -> %d\n", ret);
+    if (ret != SUCCESS) {
+        return ret;
+    }
+    
+    memset(test_region, 0, sizeof(test_region));
+    printk(" PA[0]: 0x%02x\n", test_region[0]);
+    printk("VA1[0]: 0x%02x\n", va1[0]);
+    printk("VA2[0]: 0x%02x\n", va2[0]);
+    printk("\n");
+
+    printk("0x11 -> VA1[0]\n");
+    va1[0] = 0x11;
+    memory_barrier();
+    printk("VA1[0]: 0x%02x\n", va1[0]);
+    printk("VA2[0]: 0x%02x\n", va2[0]);
+    printk(" PA[0]: 0x%02x\n", test_region[0]);
+    printk("\n");
+
+    printk("Clean & Invalidate VA1.\n");
+    aarch64_dcache_clean_invd((void *)va1);
+    printk("VA1[0]: 0x%02x\n", va1[0]);
+    printk("VA2[0]: 0x%02x\n", va2[0]);
+    printk(" PA[0]: 0x%02x\n", test_region[0]);
+    printk("\n");
+
+    printk("0x77 -> PA[0]\n");
+    printk("0x88 -> VA1[0]\n");
+    test_region[0] = 0x77;
+    memory_barrier();
+    va1[0] = 0x88;
+    printk(" PA[0]: 0x%02x\n", test_region[0]);
+    printk("VA1[0]: 0x%02x\n", va1[0]);
+    printk("VA2[0]: 0x%02x\n", va2[0]);
+    printk("\n");
+
+    printk("Done\n");
+
+    return SUCCESS;
+}
+
---
+++ drivers/src/test/aarch64/cache/cache_test.h
@@ -0,0 +1,57 @@
+/*
+ * driver/test/aarch64/mmu/cache_test.h
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#ifndef DRIVER_TEST_AARCH64_CACHE_TEST_H
+#define DRIVER_TEST_AARCH64_CACHE_TEST_H
+
+/* includes */
+
+/* defines */
+
+#ifndef ASSEMBLY
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* includes */
+
+#include <stdint.h>
+#include "lib/system/errno.h"
+#include "driver/aarch64/system_register/mair_elx.h"
+
+/* defines */
+
+#define TEST_ATTRS  (MAIR_ATTR(0, MAIR_ATTR_NORMAL_NC) |\
+                     MAIR_ATTR(1, MAIR_ATTR_NORMAL_WB) |\
+                     MAIR_ATTR(2, MAIR_ATTR_NORMAL_WBWA) |\
+                     MAIR_ATTR(3, MAIR_ATTR_NORMAL_WT) |\
+                     MAIR_ATTR(4, MAIR_ATTR_NORMAL_WTWA) |\
+                     MAIR_ATTR(5, MAIR_ATTR_DEVICE_nGnRnE))
+
+#define MMU_ATTR_NC                     0
+#define MMU_ATTR_WB                     1
+#define MMU_ATTR_WBWA                   2
+#define MMU_ATTR_WT                     3
+#define MMU_ATTR_WTWA                   4
+#define MMU_ATTR_DEVICE_nGnRnE          5
+
+/* types */
+
+/* variables */
+
+/* functions */
+
+errno_t test_aarch64_cache_init_memory_map(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ASSEMBLY */
+
+#endif /* DRIVER_TEST_AARCH64_CACHE_TEST_H */
+

9b52842c0c408aac025a7ada2fb7c1728752701e
Hidekazu Kato <hkato.sssl@gmail.com>
2019-12-03 16:15:28 +0900

    adding new API 'gic400_set_level()'.
    	modified:   drivers/inc/driver/arm/gic400.h
    	new file:   drivers/src/arm/gic400/gic400_set_level.c

---
+++ drivers/src/arm/gic400/gic400_set_level.c
@@ -0,0 +1,48 @@
+/*
+ * arm/gic400/gic400_set_level.c
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include "lib/system/errno.h"
+#include "driver/arm/device/gic400.h"
+#include "driver/arm/gic400.h"
+#include "gic400_local.h"
+
+/* defines */
+
+/* types */
+
+/* prototypes */
+
+/* variables */
+
+/* functions */
+
+static errno_t set_level(struct gic400 *gic, uint32_t level)
+{
+    uint32_t d;
+
+    d = level << gic->priority.shift_ct;
+    gic400_write_sync_cpuif(gic, GICC_PMR, d);
+
+    return SUCCESS;
+}
+
+errno_t gic400_set_level(struct gic400 *gic, uint32_t level)
+{
+    errno_t ret;
+
+    // GIC-400の仕様としてGICC_PMRに0xffを書き込む事で最低優先度が
+    // 設定される事が保証される為、パラメータ値のチェックは省略する。
+
+    if (gic != NULL) {
+        ret = set_level(gic, level);
+    } else {
+        ret = -EINVAL;
+    }
+
+    return ret;
+}

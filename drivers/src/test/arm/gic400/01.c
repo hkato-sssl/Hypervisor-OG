f1ed926e9e9410ba8b99ffa1a5b0a09e8e8fd595
Hidekazu Kato <hkato.sssl@gmail.com>
2019-12-03 16:19:19 +0900

    adding test code.
    	new file:   drivers/src/test/arm/gic400/01.c
    	new file:   drivers/src/test/arm/gic400/02.c

---
+++ drivers/src/test/arm/gic400/01.c
@@ -0,0 +1,68 @@
+/*
+ * test/arm/gic400/01.c
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+/* テスト項目：初期化処理
+ *
+ * 初期化APIを実行後、レジスタ情報を出力して実行結果を確認する。
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include "lib/system/errno.h"
+#include "lib/system/printk.h"
+#include "lib/system/stdc/string.h"
+#include "driver/arm/gic400.h"
+
+/* defines */
+
+/* test parameters */
+
+#define GIC400_DIST_BASE    0xf9010000
+#define GIC400_CPUIF_BASE   0xf9020000
+
+/* types */
+
+/* prototypes */
+
+/* variables */
+
+static struct gic400 gic;
+
+/* functions */
+
+static errno_t init(void)
+{
+    errno_t ret;
+    struct gic400_configuration config;
+
+    memset(&config, 0, sizeof(config));
+    config.base.dist = (void *)GIC400_DIST_BASE;
+    config.base.cpuif = (void *)GIC400_CPUIF_BASE;
+    ret = gic400_init(&gic, &config);
+    printk("gic400_init() -> %d\n", ret);
+
+    return ret;
+}
+
+errno_t test_arm_gic400_01(void)
+{
+    errno_t ret;
+
+    printk("<%s>\n", __func__);
+    ret = init();
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    gic400_dump_ns_cpuif(&gic);
+    gic400_dump_ns_distributor(&gic);
+
+    printk("Done\n");
+
+    return SUCCESS;
+}
+
+
---
+++ drivers/src/test/arm/gic400/02.c
@@ -0,0 +1,90 @@
+/*
+ * test/arm/gic400/02.c
+ *
+ * (C) 2019 Hidekazu Kato
+ */
+
+/* テスト項目：SGI通知(Self)
+ *
+ * 自コアに対してSGI#0-15の割り込みを発生させる。
+ * 全割り込み通知を認識する事を確認。
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include "lib/system/errno.h"
+#include "lib/system/printk.h"
+#include "lib/system/stdc/string.h"
+#include "driver/arm.h"
+#include "driver/arm/gic400.h"
+
+/* defines */
+
+/* test parameters */
+
+#define GIC400_DIST_BASE    0xf9010000
+#define GIC400_CPUIF_BASE   0xf9020000
+
+/* types */
+
+/* prototypes */
+
+/* variables */
+
+static struct gic400 gic;
+
+/* functions */
+
+static errno_t init(void)
+{
+    errno_t ret;
+    struct gic400_configuration config;
+
+    memset(&config, 0, sizeof(config));
+    config.base.dist = (void *)GIC400_DIST_BASE;
+    config.base.cpuif = (void *)GIC400_CPUIF_BASE;
+    ret = gic400_init(&gic, &config);
+    printk("gic400_init() -> %d\n", ret);
+
+    return ret;
+}
+
+errno_t test_arm_gic400_02(void)
+{
+    errno_t ret;
+    struct gic400_interrupt_configuration config;
+
+    printk("<%s>\n", __func__);
+    ret = init();
+    if (ret != SUCCESS) {
+        return ret;
+    }
+
+    gic400_dump_ns_cpuif(&gic);
+    gic400_dump_ns_distributor(&gic);
+    printk("----------\n");
+
+    arm_disable_interrupts();
+
+    memset(&config, 0, sizeof(config));
+    config.targets = 1;
+    config.priority = 1;
+    ret = gic400_configure_interrupt(&gic, 7, &config);
+    printk("gic400_configure_interrupt() -> %d\n", ret);
+
+    ret = gic400_assert_sgi(&gic, 1, 7);
+    printk("gic400_assert_sgi() -> %d\n", ret);
+    ret = gic400_enable_interrupt(&gic, 7);
+    printk("gic400_enable_interrupt() -> %d\n", ret);
+    ret = gic400_set_level(&gic, 0xff);
+    printk("gic400_set_level() -> %d\n", ret);
+
+    gic400_dump_ns_cpuif(&gic);
+    gic400_dump_ns_distributor(&gic);
+
+    printk("Done\n");
+
+    return SUCCESS;
+}
+
+

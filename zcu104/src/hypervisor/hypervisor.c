5f5b252556e841b7438f2201bb28bdcf9101e18e
Hidekazu Kato <hkato.sssl@gmail.com>
2019-12-22 02:12:28 +0900

    Thread機能対応
    	modified:   zcu104/src/hypervisor/hypervisor.c

--- zcu104/src/hypervisor/hypervisor.c
+++ zcu104/src/hypervisor/hypervisor.c
@@ -16,26 +16,11 @@
 
 /* prototypes */
 
-void *exc_stack_top(void);
+void *main_stack_top(void);
 
 /* variables */
 
 /* functions */
-
-static void init_tls(void)
-{
-    char *sp;
-    uint64_t *tls;
-
-    sp = exc_stack_top();
-    sp -= TLS_SIZE;
-
-    tls = (uint64_t *)sp;
-    memset(tls, 0, TLS_SIZE);
-    tls[TLS_EXCEPTION_SP] = (uint64_t)sp;
-
-    aarch64_write_tpidr_el2((uint64_t)tls);
-}
 
 static void init_exception(void)
 {
@@ -47,9 +32,11 @@ static void init_exception(void)
 
 void hypervisor(void)
 {
-    init_tls();
-    init_exception();
+    void test_guest_01(void);
+    void *sp;
 
-    test_guest_01();
+    init_exception();
+    sp = main_stack_top();
+    thread_launch((void *)test_guest_01, sp, NULL);
 }
 

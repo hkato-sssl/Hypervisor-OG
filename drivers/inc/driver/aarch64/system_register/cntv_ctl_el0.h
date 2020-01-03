1d0ce38abf65a5d56965494e4aba2aa0f97a9463
Hidekazu Kato <hkato.sssl@gmail.com>
2020-01-03 17:36:32 +0900

    adding a new file.
    	new file:   drivers/inc/driver/aarch64/system_register/cntv_ctl_el0.h

---
+++ drivers/inc/driver/aarch64/system_register/cntv_ctl_el0.h
@@ -0,0 +1,21 @@
+/*
+ * driver/aarch64/system_register/cntv_ctl_el0.h
+ *
+ * (C) 2020 Hidekazu Kato
+ */
+
+#ifndef DRIVER_AARCH64_SYSTEM_REGISTER_CNTV_CTL_EL0_H
+#define DRIVER_AARCH64_SYSTEM_REGISTER_CNTV_CTL_EL0_H
+
+/* includes */
+
+#include "lib/bit.h"
+
+/* defines */
+
+#define CNTV_CTL_EL0_ISTATUS        BIT(2)
+#define CNTV_CTL_EL0_IMASK          BIT(1)
+#define CNTV_CTL_EL0_ENABLE         BIT(0)
+
+#endif /* DRIVER_AARCH64_SYSTEM_REGISTER_CNTV_CTL_EL0_H */
+

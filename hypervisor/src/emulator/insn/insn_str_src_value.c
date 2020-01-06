a0a572447dec436ecf39b31775bddd888d47e83a
Hidekazu Kato <hkato.sssl@gmail.com>
2020-01-06 12:30:06 +0900

    reinmplementing a API.
    	modified:   hypervisor/src/emulator/insn/insn_str_src_value.c

--- hypervisor/src/emulator/insn/insn_str_src_value.c
+++ hypervisor/src/emulator/insn/insn_str_src_value.c
@@ -20,29 +20,17 @@
 
 /* functions */
 
-static uint64_t str_src_value(const struct insn *insn)
-{
-    uint64_t d;
-    uint64_t mask;
-
-    d = insn->vpc->regs[insn->op.str.gpr.src];
-    if (insn->op.str.size < 8) {
-        mask = (1ULL << (insn->op.str.size * 8)) - 1;
-        d &= mask;
-    }
-
-    return d;
-}
-
 uint64_t insn_str_src_value(const struct insn *insn)
 {
     uint64_t val;
+    uint64_t mask;
 
-    if (insn->type == INSN_TYPE_STR) {
-        val = str_src_value(insn);
-    } else {
-        SYSTEM_ASSERT(0);
-        val = 0;
+    SYSTEM_ASSERT(insn->type == INSN_TYPE_STR);
+
+    val = insn->vpc->regs[insn->op.str.gpr.src];
+    if (insn->op.str.size < 8) {
+        mask = (1ULL << (insn->op.str.size * 8)) - 1;
+        val &= mask;
     }
 
     return val;

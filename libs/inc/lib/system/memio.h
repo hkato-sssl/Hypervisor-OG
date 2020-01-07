0d315cdae92cfe60c1f5a0dede13f3bbf27e00c4
Hidekazu Kato <hkato.sssl@gmail.com>
2020-01-07 17:36:08 +0900

    reimplementing the I/O library.
    	modified:   libs/inc/lib/system/memio.h
    	new file:   libs/src/system/reg_write_sync.S

--- libs/inc/lib/system/memio.h
+++ libs/inc/lib/system/memio.h
@@ -23,37 +23,86 @@ extern "C" {
 
 /* defines */
 
+#define REG_READ64(b, r)            reg_read64((uintptr_t)(b), (uintptr_t)(r))
+#define REG_READ32(b, r)            reg_read32((uintptr_t)(b), (uintptr_t)(r))
+#define REG_READ16(b, r)            reg_read16((uintptr_t)(b), (uintptr_t)(r))
+#define REG_READ8(b, r)             reg_read8((uintptr_t)(b), (uintptr_t)(r))
+
+#define REG_WRITE64(b, r, d)        reg_write64((uintptr_t)(b), (uintptr_t)(r), (uint64_t)(d))
+#define REG_WRITE32(b, r, d)        reg_write32((uintptr_t)(b), (uintptr_t)(r), (uint32_t)(d))
+#define REG_WRITE16(b, r, d)        reg_write16((uintptr_t)(b), (uintptr_t)(r), (uint16_t)(d))
+#define REG_WRITE8(b, r, d)         reg_write8((uintptr_t)(b), (uintptr_t)(r), (uint8_t)(d))
+
+#define REG_WRITE64_SYNC(b, r, d)   reg_write64_sync((uintptr_t)(b), (uintptr_t)(r), (uint64_t)(d))
+#define REG_WRITE32_SYNC(b, r, d)   reg_write32_sync((uintptr_t)(b), (uintptr_t)(r), (uint32_t)(d))
+#define REG_WRITE16_SYNC(b, r, d)   reg_write16_sync((uintptr_t)(b), (uintptr_t)(r), (uint16_t)(d))
+#define REG_WRITE8_SYNC(b, r, d)    reg_write8_sync((uintptr_t)(b), (uintptr_t)(r), (uint8_t)(d))
+
 /* types */
 
 /* variables */
 
-/* functions */
-
-/*
- * ARMを利用しているシステムではレジスタ領域がDevice Memoryとして適切
- * にマッピングされる事を前提としている為、明示的にDMB/DSBの実行は行っ
- * ていない。
- */
-
-#define REG_WRITE64(b, r, d)   (*(volatile uint64_t *)((uintptr_t)(b) + (uintptr_t)(r)) = (uint64_t)(d))
-#define REG_WRITE32(b, r, d)   (*(volatile uint32_t *)((uintptr_t)(b) + (uintptr_t)(r)) = (uint32_t)(d))
-#define REG_WRITE16(b, r, d)   (*(volatile uint16_t *)((uintptr_t)(b) + (uintptr_t)(r)) = (uint16_t)(d))
-#define REG_WRITE8(b, r, d)    (*(volatile uint8_t *)((uintptr_t)(b) + (uintptr_t)(r)) = (uint8_t)(d))
-
-#define REG_READ64(b, r)       (*(volatile uint64_t *)((uintptr_t)(b) + (uintptr_t)(r)))
-#define REG_READ32(b, r)       (*(volatile uint32_t *)((uintptr_t)(b) + (uintptr_t)(r)))
-#define REG_READ16(b, r)       (*(volatile uint16_t *)((uintptr_t)(b) + (uintptr_t)(r)))
-#define REG_READ8(b, r)        (*(volatile uint8_t *)((uintptr_t)(b) + (uintptr_t)(r)))
-
-#define REG_WRITE64_SYNC(b, r, d)   (REG_WRITE64((b), (r), (d)), REG_READ64((b), (r)))
-#define REG_WRITE32_SYNC(b, r, d)   (REG_WRITE32((b), (r), (d)), REG_READ32((b), (r)))
-#define REG_WRITE16_SYNC(b, r, d)   (REG_WRITE16((b), (r), (d)), REG_READ16((b), (r)))
-#define REG_WRITE8_SYNC(b, r, d)    (REG_WRITE8((b), (r), (d)), REG_READ8((b), (r)))
+/* inline functions */
 
 static inline void memory_barrier(void)
 {
-	__asm volatile ("dsb osh" ::: "memory");
+	__asm volatile ("dmb osh" ::: "memory");
 }
+
+static inline uint64_t reg_read64(uintptr_t base, uintptr_t reg)
+{
+    memory_barrier();
+    return *(volatile uint64_t *)(base + reg);
+}
+
+static inline uint64_t reg_read32(uintptr_t base, uintptr_t reg)
+{
+    memory_barrier();
+    return *(volatile uint32_t *)(base + reg);
+}
+
+static inline uint64_t reg_read16(uintptr_t base, uintptr_t reg)
+{
+    memory_barrier();
+    return *(volatile uint16_t *)(base + reg);
+}
+
+static inline uint64_t reg_read8(uintptr_t base, uintptr_t reg)
+{
+    memory_barrier();
+    return *(volatile uint8_t *)(base + reg);
+}
+
+static inline void reg_write64(uintptr_t base, uintptr_t reg, uint64_t d)
+{
+    memory_barrier();
+    *(volatile uint64_t *)(base + reg) = d;
+}
+
+static inline void reg_write32(uintptr_t base, uintptr_t reg, uint32_t d)
+{
+    memory_barrier();
+    *(volatile uint32_t *)(base + reg) = d;
+}
+
+static inline void reg_write16(uintptr_t base, uintptr_t reg, uint16_t d)
+{
+    memory_barrier();
+    *(volatile uint16_t *)(base + reg) = d;
+}
+
+static inline void reg_write8(uintptr_t base, uintptr_t reg, uint8_t d)
+{
+    memory_barrier();
+    *(volatile uint8_t *)(base + reg) = d;
+}
+
+/* functions */
+
+void reg_write64_sync(uintptr_t base, uintptr_t reg, uint64_t d);
+void reg_write32_sync(uintptr_t base, uintptr_t reg, uint32_t d);
+void reg_write16_sync(uintptr_t base, uintptr_t reg, uint16_t d);
+void reg_write8_sync(uintptr_t base, uintptr_t reg, uint8_t d);
 
 #ifdef __cplusplus
 }

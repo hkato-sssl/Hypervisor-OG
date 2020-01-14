f53dcf49e15b1321eb5a2e84b8e1f99e0e8e2d37
Hidekazu Kato <hkato.sssl@gmail.com>
2020-01-14 13:48:44 +0900

    専用APIを利用する形式に修正
    	modified:   hypervisor/src/test/vm/emulator_02.c

--- hypervisor/src/test/vm/emulator_02.c
+++ hypervisor/src/test/vm/emulator_02.c
@@ -19,20 +19,12 @@
 
 /* functions */
 
-void print_insn(const struct insn *insn)
-{
-    printk(" Access : %s\n", ((insn->type == INSN_TYPE_STR) ? "STR" : "LDR"));
-    printk("Address : %018p\n", insn->op.ldr.ipa);
-    printk("   Size : %u\n", insn->op.ldr.size);
-    printk("   Sign : %u\n", insn->op.ldr.flag.sign);
-}
-
-errno_t emulator_02(struct vpc *vpc, const struct insn *insn)
+errno_t emulator_02(const struct insn *insn, void *arg)
 {
     errno_t ret;
 
     printk("<%s>\n", __func__);
-    print_insn(insn);
+    insn_print(insn);
 
     ret = -ENOSYS;
 

04f464138b9370ceed1832fc75d0ee9cc411adb7
Hidekazu Kato <hkato.sssl@gmail.com>
2020-01-16 18:06:43 +0900

    レジスタ値が正常に取得できない問題を訂正
    	modified:   hypervisor/src/emulator/insn/insn_str_src_value.c

--- hypervisor/src/emulator/insn/insn_str_src_value.c
+++ hypervisor/src/emulator/insn/insn_str_src_value.c
@@ -23,20 +23,15 @@
 uint64_t insn_str_src_value(const struct insn *insn)
 {
     uint64_t val;
-    uint64_t mask;
     uint8_t gpr;
 
     SYSTEM_ASSERT(insn->type == INSN_TYPE_STR);
     
     gpr = insn->op.str.gpr.src;
     if (gpr < 31) {
-        val = insn->vpc->regs[gpr];
+        val = insn->vpc->regs[VPC_X0 + gpr];
     } else {
         val = 0;
-    }
-    if (insn->op.str.size < 8) {
-        mask = (1ULL << (insn->op.str.size * 8)) - 1;
-        val &= mask;
     }
 
     return val;

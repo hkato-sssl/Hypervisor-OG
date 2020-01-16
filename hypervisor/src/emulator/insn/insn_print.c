cec1596ddff8f47ca356c8a24a60c3f8066183f8
Hidekazu Kato <hkato.sssl@gmail.com>
2020-01-16 23:44:44 +0900

    出力形式修正
    	modified:   hypervisor/src/emulator/insn/insn_print.c

--- hypervisor/src/emulator/insn/insn_print.c
+++ hypervisor/src/emulator/insn/insn_print.c
@@ -46,17 +46,17 @@ static void print_ldr(const struct insn *insn)
         }
     }
 
-    if ((insn->op.ldr.size < 8) && (insn->op.ldr.flag.sign == 0)) {
-        if (insn->op.ldr.gpr.dst == 31) {
-            printk("   dst: WZR\n");
-        } else {
-            printk("   dst: W%u\n", insn->op.ldr.gpr.dst);
-        }
-    } else {
+    if (insn->op.ldr.flag.wreg == 0) {
         if (insn->op.ldr.gpr.dst == 31) {
             printk("   dst: XZR\n");
         } else {
             printk("   dst: X%u\n", insn->op.ldr.gpr.dst);
+        }
+    } else {
+        if (insn->op.ldr.gpr.dst == 31) {
+            printk("   dst: WZR\n");
+        } else {
+            printk("   dst: W%u\n", insn->op.ldr.gpr.dst);
         }
     }
     printk("  flag: isv=%u, wreg=%u, sign=%u, post=%u, pre=%u\n", insn->op.ldr.flag.isv, insn->op.ldr.flag.wreg, insn->op.ldr.flag.sign, insn->op.ldr.flag.post, insn->op.ldr.flag.pre);
@@ -66,32 +66,32 @@ static void print_ldr(const struct insn *insn)
 static void print_str(const struct insn *insn)
 {
     printk("<INSN_TYPE_STR>\n");
-    if (insn->op.str.size < 8) {
-        if (insn->op.str.gpr.src == 31) {
-            printk("   src: WZR(0x%016llx)\n", insn_str_src_value(insn));
-        } else {
-            printk("   src: W%u(0x%016llx)\n", insn->op.str.gpr.src, insn_str_src_value(insn));
-        }
-    } else {
-        if (insn->op.str.gpr.src == 31) {
-            printk("   src: XZR(0x%016llx)\n", insn_str_src_value(insn));
-        } else {
-            printk("   src: X%u(0x%016llx)\n", insn->op.str.gpr.src, insn_str_src_value(insn));
-        }
-    }
-    if (insn->op.str.flag.isv == 0) {
-        if (insn->op.str.gpr.src == 31) {
-            printk("   dst: SP\n");
-        } else {
-            printk("   dst: X%u\n", insn->op.str.gpr.src);
-        }
-    }
     printk("    va: 0x%016llx\n", insn->op.str.va);
     printk("   ipa: 0x%016llx\n", insn->op.str.ipa);
     if (insn->op.str.flag.isv == 0) {
         printk("offset: 0x%016llx(%lld)\n", insn->op.str.offset, insn->op.str.offset);
     }
     printk("  size: %u\n", insn->op.str.size);
+    if (insn->op.str.flag.wreg == 0) {
+        if (insn->op.str.gpr.src == 31) {
+            printk("   src: XZR(0x%016llx)\n", insn_str_src_value(insn));
+        } else {
+            printk("   src: X%u(0x%016llx)\n", insn->op.str.gpr.src, insn_str_src_value(insn));
+        }
+    } else {
+        if (insn->op.str.gpr.src == 31) {
+            printk("   src: WZR(0x%016llx)\n", insn_str_src_value(insn));
+        } else {
+            printk("   src: W%u(0x%016llx)\n", insn->op.str.gpr.src, insn_str_src_value(insn));
+        }
+    }
+    if (insn->op.str.flag.isv == 0) {
+        if (insn->op.str.gpr.dst == 31) {
+            printk("   dst: SP\n");
+        } else {
+            printk("   dst: X%u\n", insn->op.str.gpr.dst);
+        }
+    }
     printk("  flag: isv=%u, wreg=%u, sign=%u, post=%u, pre=%u\n", insn->op.str.flag.isv, insn->op.str.flag.wreg, insn->op.str.flag.sign, insn->op.str.flag.post, insn->op.str.flag.pre);
     printk("\n");
 }

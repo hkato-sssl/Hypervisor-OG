16fa9d25840f23f480053d11f94d6dbe3ae4c86b
Hidekazu Kato <hkato.sssl@gmail.com>
2020-03-03 16:10:06 +0900

    adding a new file.
    	new file:   drivers/inc/driver/arm/device/smmu500.h

---
+++ drivers/inc/driver/arm/device/smmu500.h
@@ -0,0 +1,17 @@
+/*
+ * driver/arm/device/smmu500.h
+ *
+ * (C) 2020 Hidekazu Kato
+ */
+
+#ifndef DRIVER_ARM_DEVICE_SMMU500_H
+#define DRIVER_ARM_DEVICE_SMMU500_H
+
+/* includes */
+
+#include "driver/arm/device/smmuv2.h"
+
+/* defines */
+
+#endif /* DRIVER_ARM_DEVICE_SMMU500_H */
+

0922fc9cdb843008a22ac30b5b0d174e21039c7d
Hidekazu Kato <hkato.sssl@gmail.com>
2020-05-11 18:27:16 +0900

    IRQ番号定義を追加
    	modified:   drivers/inc/driver/xilinx/device/mpsoc.h

--- drivers/inc/driver/xilinx/device/mpsoc.h
+++ drivers/inc/driver/xilinx/device/mpsoc.h
@@ -12,14 +12,20 @@
 /* defines */
 
 #define NR_SOC_CPUS         4
+
 #define REG_GIC400D         0xf9010000
 #define REG_GIC400C         0xf9020000
 #define REG_GIC400H         0xf9040000
 #define REG_GIC400V         0xf9060000
 #define REG_SMMU500         0xfd800000
-
 #define REG_UART0           0xff000000
 #define REG_UART1           0xff010000
+
+/* interrupt No. */
+
+#define IRQ_UART0           53
+#define IRQ_UART1           54
+#define IRQ_SMMU500         187
 
 #endif /* DRIVER_XILINX_DEVICE_MPSOC_H */
 

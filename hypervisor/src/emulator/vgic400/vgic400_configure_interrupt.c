89b6febd91d4b4e69311d95572c5e215f49ed655
Hidekazu Kato <hkato.sssl@gmail.com>
2020-05-13 15:58:26 +0900

    API仕様変更
    PPIが指定された場合は全コアについて同一の設定を反映させる仕様に変更。
    初期化処理が冗長的になった事が修正を実施した理由。
    データ構成に変更は無いので、PPIに関してプロセッサ固有の設定が必要になった場合は、専用APIを追加する事で対応可能である。
    
    	modified:   inc/hypervisor/emulator/vgic400.h
    	modified:   src/emulator/vgic400/vgic400_configure_interrupt.c

--- hypervisor/src/emulator/vgic400/vgic400_configure_interrupt.c
+++ hypervisor/src/emulator/vgic400/vgic400_configure_interrupt.c
@@ -7,7 +7,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "lib/bit.h"
-#include "driver/arm/gic400.h"
+#include "hypervisor/vm.h"
 #include "hypervisor/emulator/vgic400.h"
 
 /* defines */
@@ -20,9 +20,11 @@
 
 /* functions */
 
-static errno_t configure_interrupt(struct vgic400 *vgic, struct vpc *vpc, const struct vgic400_interrupt_configuration *config)
+static errno_t configure_interrupt(struct vgic400 *vgic, const struct vgic400_interrupt_configuration *config)
 {
     uint32_t d;
+    uint32_t i;
+    uint32_t idx;
 
     d = 1 << 28;        /* State = pending */
     if (config->flag.hw != 0) {
@@ -35,7 +37,10 @@ static errno_t configure_interrupt(struct vgic400 *vgic, struct vpc *vpc, const 
     d |= (uint32_t)(config->virtual_id);
 
     if (config->virtual_id < 32) {
-        vgic->ppi[vpc->proc_no].template[config->virtual_id - 16] = d;
+        idx = config->virtual_id - 16;
+        for (i =0; i < vgic->vm->nr_procs; ++i) {
+            vgic->ppi[i].template[idx] = d;
+        }
     } else {
         vgic->spi.template[config->virtual_id - 32] = d;
     }
@@ -71,13 +76,13 @@ static errno_t validate_configuration(const struct vgic400_interrupt_configurati
     return ret;
 }
 
-errno_t vgic400_configure_interrupt(struct vgic400 *vgic, struct vpc *vpc, const struct vgic400_interrupt_configuration *config)
+errno_t vgic400_configure_interrupt(struct vgic400 *vgic, const struct vgic400_interrupt_configuration *config)
 {
     errno_t ret;
 
     ret = validate_configuration(config);
     if (ret == SUCCESS) {
-        ret = configure_interrupt(vgic, vpc, config);
+        ret = configure_interrupt(vgic, config);
     }
 
     return ret;

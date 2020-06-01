c3dbfbc85d135f76e86aff165c9ef88b35fc3774
Hidekazu Kato <hkato.sssl@gmail.com>
2020-06-01 22:21:23 +0900

    Root deviceとしてUSBを利用するconfigurationを追加
    	new file:   zcu104-linux/src/hypervisor/guest/guest_usb_linux.c
    	new file:   zcu104-linux/src/hypervisor/guest/guest_usb_linux_device.c

---
+++ zcu104-linux/src/hypervisor/guest/guest_usb_linux.c
@@ -0,0 +1,130 @@
+/*
+ * hypervisor/guest/guest_usb_linux.c
+ *
+ * (C) 2020 Hidekazu Kato
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include "lib/system/errno.h"
+#include "lib/system/printk.h"
+#include "driver/arm/gic400.h"
+#include "driver/arm/smmu500.h"
+#include "driver/aarch64/mmu.h"
+#include "hypervisor/vm.h"
+#include "hypervisor/vpc.h"
+#include "hypervisor/mmu.h"
+#include "hypervisor/soc/xilinx/mpsoc.h"
+
+/* defines */
+
+#define NR_CPUS             1
+#define GUEST_VMID          2
+
+/* types */
+
+/* prototypes */
+
+static errno_t el2_irq_handler(struct vpc *vpc, struct vgic400 *vgic, uint32_t iar);
+
+/* variables */
+
+extern struct gic400 sys_gic;
+extern struct smmu500 sys_smmu;
+extern struct aarch64_mmu sys_mmu;
+extern struct aarch64_mmu_block_pool sys_pool;
+
+extern uint16_t nr_guest_usb_linux_devices;
+extern struct soc_device *guest_usb_linux_devices[];
+extern uint16_t nr_guest_usb_linux_streams;
+extern const struct smmu_stream *guest_usb_linux_streams[];
+
+static DESC_XILINX_MPSOC_STAGE2_LEVEL1_TABLE(table);
+
+static struct xilinx_mpsoc mpsoc;
+static struct vpc vpcs[NR_CPUS];
+static uint64_t regs[NR_CPUS][NR_VPC_REGS] __attribute__ ((aligned(32)));
+static struct vpc_exception_ops ops;
+static struct vgic400_interrupt_ops interrupt_ops = {
+    .maintenance = vgic400_operate_maintenance_interrupt,
+    .el1.sgi = vgic400_inject_sgi,
+    .el1.ppi = vgic400_inject_interrupt,
+    .el1.spi = vgic400_inject_interrupt,
+    .el2.spi = el2_irq_handler,
+};
+
+/* functions */
+
+static errno_t emulate_hvc(struct vpc *vpc)
+{
+    gic400_dump_ns_cpuif(&sys_gic);
+    gic400_dump_ns_distributor(&sys_gic);
+    smmu500_dump(&sys_smmu);
+    smmu500_dump_stream_match_register(&sys_smmu, 0);
+
+    return -ENOSYS;
+}
+
+static errno_t el2_irq_handler(struct vpc *vpc, struct vgic400 *vgic, uint32_t iar)
+{
+    struct xilinx_mpsoc *chip;
+
+    printk("<%s>\n", __func__);
+
+    if (iar == IRQ_SMMU500) {
+        chip = vpc->vm->soc->chip;
+        smmu500_dump(chip->smmu.device);
+    } else {
+        printk("Unrecognized IRQ#%u has occured.\n", iar);
+    }
+
+    return -ENOTSUP;
+}
+
+static void *init_mpsoc(void)
+{
+    errno_t ret;
+    uint32_t i;
+    struct xilinx_mpsoc_configuration config;
+
+    memset(&config, 0, sizeof(config));
+
+    for (i = 0; i < NR_CPUS; ++i) {
+        config.vpc.vpcs[i] = &(vpcs[i]);
+        config.vpc.register_arrays[i] = regs[i];
+    }
+
+    ops = *xilinx_mpsoc_default_vpc_exception_ops();
+    ops.aarch64.hvc = emulate_hvc;
+    config.vpc.ops = &ops;
+
+    config.vmid = GUEST_VMID;
+    config.nr_procs = NR_CPUS;
+    config.stage2.pool = &sys_pool;
+    config.stage2.level1_table = table;
+    config.gic.device = &sys_gic;
+    config.gic.nr_sgis = 8;
+    for (i = 0; i < 8; ++i) {
+        config.gic.sgis[i] = (uint16_t)i;
+    }
+    config.gic.nr_ppis = 2;
+    config.gic.ppis[0] = 27;
+    config.gic.ppis[1] = 30;
+    config.gic.ops = &interrupt_ops;
+    config.smmu.device = &sys_smmu;
+    config.smmu.nr_streams = nr_guest_usb_linux_streams;
+    config.smmu.streams = guest_usb_linux_streams;
+    config.smmu.flag.fault = 1;
+    config.nr_devices = nr_guest_usb_linux_devices;
+    config.devices = guest_usb_linux_devices;
+
+    ret = xilinx_mpsoc_initialize(&mpsoc, &config);
+
+    return (ret == SUCCESS) ? &mpsoc : NULL;
+}
+
+void *guest_usb_linux(void)
+{
+    return init_mpsoc();
+}
+
---
+++ zcu104-linux/src/hypervisor/guest/guest_usb_linux_device.c
@@ -0,0 +1,206 @@
+/*
+ * hypervisor/guest/guest_usb_linux_device.c
+ *
+ * (C) 2020 Hidekazu Kato
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include "driver/arm/smmu500.h"
+#include "driver/aarch64/stage2.h"
+#include "hypervisor/soc.h"
+
+/* defines */
+
+#define RAM_START_PA        0x00000000  /* 1GB */
+#define RAM_START_IPA       0x00000000
+#define RAM_SIZE            0x40000000
+
+#define UART_IPA            0xff000000  /* PS UART0 */
+#define UART_PA             0xff000000
+#define UART_SIZE           4096
+
+#define AXI_SMMU_TEST_IPA   0xa0002000
+#define AXI_SMMU_TEST_PA    0xa0002000
+#define AXI_SMMU_TEST_SIZE  4096
+
+/* types */
+
+/* prototypes */
+
+/* variables */
+
+static struct soc_device ram = {
+    .nr_irqs = 0,
+    .irqs = NULL,
+    .region.pa = RAM_START_PA,
+    .region.ipa = RAM_START_IPA,
+    .region.size = RAM_SIZE,
+    .region.memory_type = HYP_MMU_MT_NORMAL_WB,
+    .region.shareability = HYP_MMU_SH_ISH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 1,
+    .region.access.flag.exec = 1
+};
+
+static struct soc_device_interrupt uart0_irqs[] = { { 53, 53}  };
+static struct soc_device uart0 = {
+    .nr_irqs = 1,
+    .irqs = uart0_irqs,
+    .region.pa = UART_PA,
+    .region.ipa = UART_IPA,
+    .region.size = UART_SIZE,
+    .region.memory_type = HYP_MMU_MT_DEVICE_nGnRE,
+    .region.shareability = HYP_MMU_SH_OSH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 1,
+    .region.access.flag.exec = 0
+};
+
+static struct soc_device_interrupt rtc_irqs[] = { { 58, 58} , { 59, 59 } };
+static struct soc_device rtc = {
+    .nr_irqs = 2,
+    .irqs = rtc_irqs,
+    .region.pa = 0xffa60000,
+    .region.ipa = 0xffa60000,
+    .region.size = 0x1000,
+    .region.memory_type = HYP_MMU_MT_DEVICE_nGnRE,
+    .region.shareability = HYP_MMU_SH_OSH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 1,
+    .region.access.flag.exec = 0
+};
+
+static struct soc_device ddrc = {
+    .nr_irqs = 0,
+    .irqs = NULL,
+    .region.pa = 0xfd070000,
+    .region.ipa = 0xfd070000,
+    .region.size = 0x1000,
+    .region.memory_type = HYP_MMU_MT_DEVICE_nGnRE,
+    .region.shareability = HYP_MMU_SH_OSH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 0,
+    .region.access.flag.exec = 0
+};
+
+static struct soc_device_interrupt gem3_irqs[] = { { 95, 95 } };
+static struct soc_device gem3 = {
+    .nr_irqs = 1,
+    .irqs = gem3_irqs,
+    .region.pa = 0xff0e0000,
+    .region.ipa = 0xff0e0000,
+    .region.size = 0x1000,
+    .region.memory_type = HYP_MMU_MT_DEVICE_nGnRE,
+    .region.shareability = HYP_MMU_SH_OSH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 1,
+    .region.access.flag.exec = 0
+};
+
+static struct soc_device_interrupt sd1_irqs[] = { { 81, 81 } };
+static struct soc_device sd1 = {
+    .nr_irqs = 1,
+    .irqs = sd1_irqs,
+    .region.pa = 0xff170000,
+    .region.ipa = 0xff170000,
+    .region.size = 0x1000,
+    .region.memory_type = HYP_MMU_MT_DEVICE_nGnRE,
+    .region.shareability = HYP_MMU_SH_OSH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 1,
+    .region.access.flag.exec = 0
+};
+
+static struct soc_device xhci0_reg = {
+    .nr_irqs = 0,
+    .irqs = NULL,
+    .region.pa = 0xff9d0000,
+    .region.ipa = 0xff9d0000,
+    .region.size = 0x1000,
+    .region.memory_type = HYP_MMU_MT_DEVICE_nGnRE,
+    .region.shareability = HYP_MMU_SH_OSH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 1,
+    .region.access.flag.exec = 0
+};
+
+static struct soc_device_interrupt xhci0_irqs[] = { { 97, 97 } };
+static struct soc_device xhci0 = {
+    .nr_irqs = 1,
+    .irqs = xhci0_irqs,
+    .region.pa = 0xfe200000,
+    .region.ipa = 0xfe200000,
+    .region.size = 0x10000,
+    .region.memory_type = HYP_MMU_MT_DEVICE_nGnRE,
+    .region.shareability = HYP_MMU_SH_OSH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 1,
+    .region.access.flag.exec = 0
+};
+
+static struct soc_device serdes0 = {
+    .nr_irqs = 0,
+    .irqs = NULL,
+    .region.pa = 0xfd400000,
+    .region.ipa = 0xfd400000,
+    .region.size = 0x20000,
+    .region.memory_type = HYP_MMU_MT_DEVICE_nGnRE,
+    .region.shareability = HYP_MMU_SH_OSH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 1,
+    .region.access.flag.exec = 0
+};
+
+static struct soc_device_interrupt i2c1_irqs[] = { { 50, 50 } };
+static struct soc_device i2c1 = {
+    .nr_irqs = 1,
+    .irqs = i2c1_irqs,
+    .region.pa = 0xff030000,
+    .region.ipa = 0xff030000,
+    .region.size = 0x1000,
+    .region.memory_type = HYP_MMU_MT_DEVICE_nGnRE,
+    .region.shareability = HYP_MMU_SH_OSH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 1,
+    .region.access.flag.exec = 0
+};
+
+static struct soc_device axi_smmu_test = {
+    .nr_irqs = 0,
+    .irqs = NULL,
+    .region.pa = AXI_SMMU_TEST_PA,
+    .region.ipa = AXI_SMMU_TEST_IPA,
+    .region.size = AXI_SMMU_TEST_SIZE,
+    .region.memory_type = HYP_MMU_MT_DEVICE_nGnRE,
+    .region.shareability = HYP_MMU_SH_OSH,
+    .region.access.flag.read = 1,
+    .region.access.flag.write = 1,
+    .region.access.flag.exec = 0
+};
+
+uint16_t nr_guest_usb_linux_devices = 10;
+struct soc_device *guest_usb_linux_devices[] = {
+    &ram,
+    &uart0,
+    &rtc,
+    &ddrc,
+    &sd1,
+    &xhci0_reg,
+    &xhci0,
+    &serdes0,
+    &gem3,
+    &i2c1,
+    &axi_smmu_test,
+};
+
+static struct smmu_stream stream = {
+    .mask = 0xffff,
+    .id = 0x0e80
+};
+
+uint16_t nr_guest_usb_linux_streams = 1;
+struct smmu_stream *guest_usb_linux_streams[] = {
+    &stream
+};
+

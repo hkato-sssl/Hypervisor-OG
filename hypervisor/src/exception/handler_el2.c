dba9f1a794b5cae61a23b4f66d9955a08a3e6ee3
Hidekazu Kato <hkato.sssl@gmail.com>
2020-06-03 19:20:06 +0900

    排他制御とコア情報の出力処理を追加
    	modified:   hypervisor/src/exception/handler_el2.c

--- hypervisor/src/exception/handler_el2.c
+++ hypervisor/src/exception/handler_el2.c
@@ -7,6 +7,8 @@
 #include <stdint.h>
 #include "lib/bit.h"
 #include "lib/system/printk.h"
+#include "lib/system/spin_lock.h"
+#include "driver/aarch64.h"
 #include "driver/aarch64/exception.h"
 
 /* defines */
@@ -19,12 +21,29 @@ void exception_handler_el2(uint64_t *ei) __attribute__((weak));
 
 /* variables */
 
+static spin_lock_t lock;
+
 /* functions */
 
 void exception_handler_el2(uint64_t *ei)
 {
-    printk("<%s>\n", __func__);
+    uint32_t i;
+    uint64_t *sp;
+
+    spin_lock(&lock);
+
+    printk("<%s#%u>\n", __func__, aarch64_cpu_no());
     aarch64_exception_dump(ei);
+    printk("\n");
+
+    sp = (uint64_t *)(ei[EXC_SP]);
+    for (i = 0; i < 64; i += 2) {
+        printk("%04x: %016llx %016llx\n", (i * 8), sp[i], sp[i + 1]);
+    };
+
+    printk("\n");
+
+    spin_unlock(&lock);
 
     for (;;);
 }

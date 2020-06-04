0ac4b40074392730da9a8e68729836481e2d3aad
Hidekazu Kato <hkato.sssl@gmail.com>
2020-06-04 15:08:51 +0900

    thread library API変更に伴う修正
    Hypervisorで最初に起動される関数をhypervisor()に変更
    
    	renamed:    zcu104-linux/src/hypervisor/hypervisor_main.c -> zcu104-linux/src/hypervisor/hypervisor.c
    	modified:   zcu104-linux/src/hypervisor/hypervisor_start.S

--- zcu104-linux/src/hypervisor/hypervisor.c
+++ zcu104-linux/src/hypervisor/hypervisor.c
@@ -1,5 +1,5 @@
 /*
- * hypervisor_main.c
+ * hypervisor.c
  *
  * (C) 2019 Hidekazu Kato
  */
@@ -52,18 +52,42 @@ static void launch_guest(struct xilinx_mpsoc *chip)
 void hypervisor_main(void)
 {
     struct xilinx_mpsoc *chip;
+    struct thread_parameter parameter;
 
     printk("CPU#%u\n", cpu_no());
 
     gic400_enable_interrupt(&sys_gic, IRQ_SMMU500);
     gic400_set_priority_mask(&sys_gic, 0xff);
 
+    memset(&parameter, 0, sizeof(parameter));
+
     chip = guest_linux();
-    thread_launch_at(1, (thread_entry_t)launch_guest, chip);
+    parameter.entry = (thread_entry_t)launch_guest;
+    parameter.args[0] = (uintptr_t)chip;
+    thread_launch(1, &parameter);
 
     chip = guest_usb_linux();
-    thread_launch_at(3, (thread_entry_t)launch_guest, chip);
+    parameter.args[0] = (uintptr_t)chip;
+    thread_launch(3, &parameter);
 
     thread_terminate();
+}
+
+void hypervisor(void)
+{
+    /* NOTE:
+     * Hypervisor 起動時は Primary Processor により本関数が最初に呼ばれる。
+     * この時、全ての Secondary Processor は Standby 状態にある。
+     * 本関数実行時は thread としては起動していないので TLS へアクセスする
+     * API は実行不可能。
+     * 本関数の役割は H/W に関する初期化処理や thread の生成となる。
+     */ 
+
+    struct thread_parameter parameter;
+
+    printk("<%s>\n", __func__);
+    memset(&parameter, 0, sizeof(parameter));
+    parameter.entry = (thread_entry_t)hypervisor_main;
+    thread_launch_self(&parameter);
 }
 

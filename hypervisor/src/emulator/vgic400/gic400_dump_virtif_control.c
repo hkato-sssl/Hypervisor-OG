22b630f481ffadd8bedf9e6eae3b29383d8c21d7
Hidekazu Kato <hkato.sssl@gmail.com>
2020-07-19 23:17:23 +0900

    出力形式修正
    	modified:   hypervisor/src/emulator/vgic400/gic400_dump_virtif_control.c

--- hypervisor/src/emulator/vgic400/gic400_dump_virtif_control.c
+++ hypervisor/src/emulator/vgic400/gic400_dump_virtif_control.c
@@ -26,15 +26,15 @@ void gic400_dump_virtif_control(struct vgic400 *vgic)
 
     printk("<GIC-400 Virtual interface control>\n");
 
-    printk("GICH_HCR  : 0x%08x\n", gic400_read_virtif_control(vgic, GICH_HCR));
-    printk("GICH_VTR  : 0x%08x\n", gic400_read_virtif_control(vgic, GICH_VTR));
-    printk("GICH_VMCR : 0x%08x\n", gic400_read_virtif_control(vgic, GICH_VMCR));
-    printk("GICH_MISR : 0x%08x\n", gic400_read_virtif_control(vgic, GICH_MISR));
-    printk("GICH_EISR0: 0x%08x\n", gic400_read_virtif_control(vgic, GICH_EISR0));
-    printk("GICH_ELSR0: 0x%08x\n", gic400_read_virtif_control(vgic, GICH_ELSR0));
-    printk("GICH_APR0 : 0x%08x\n", gic400_read_virtif_control(vgic, GICH_APR0));
+    printk("   GICH_HCR: 0x%08x\n", gic400_read_virtif_control(vgic, GICH_HCR));
+    printk("   GICH_VTR: 0x%08x\n", gic400_read_virtif_control(vgic, GICH_VTR));
+    printk("  GICH_VMCR: 0x%08x\n", gic400_read_virtif_control(vgic, GICH_VMCR));
+    printk("  GICH_MISR: 0x%08x\n", gic400_read_virtif_control(vgic, GICH_MISR));
+    printk(" GICH_EISR0: 0x%08x\n", gic400_read_virtif_control(vgic, GICH_EISR0));
+    printk("GICH_ELRSR0: 0x%08x\n", gic400_read_virtif_control(vgic, GICH_ELRSR0));
+    printk("  GICH_APR0: 0x%08x\n", gic400_read_virtif_control(vgic, GICH_APR0));
     for (i = 0; i < 4; ++i) {
-        printk("GICH_LR%d  : 0x%08x\n", i, gic400_read_virtif_control(vgic, GICH_LR(i)));
+        printk("   GICH_LR%d: 0x%08x\n", i, gic400_read_virtif_control(vgic, GICH_LR(i)));
     }
     printk("\n");
 }

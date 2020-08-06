2253cb3e712cf9d048b3433bb590292fd25f5d0f
Hidekazu Kato <hkato.sssl@gmail.com>
2020-08-06 13:05:38 +0900

    パラメータチェック処理を追加
    	modified:   hypervisor/src/vm/vm_proc_no.c

--- hypervisor/src/vm/vm_proc_no.c
+++ hypervisor/src/vm/vm_proc_no.c
@@ -19,11 +19,27 @@
 
 uint16_t vm_virtual_proc_no(struct vm *vm, uint16_t physical_no)
 {
-    return vm->proc_map.virtual[physical_no];
+    uint16_t no;
+
+    if (physical_no < MAX_NR_VM_PROCESSORS) {
+        no = vm->proc_map.virtual[physical_no];
+    } else {
+        no = VM_NO_ASSIGN;
+    }
+
+    return no;
 }
 
 uint16_t vm_physical_proc_no(struct vm *vm, uint16_t virtual_no)
 {
-    return vm->proc_map.physical[virtual_no];
+    uint16_t no;
+
+    if (virtual_no < MAX_NR_VM_PROCESSORS) {
+        no = vm->proc_map.physical[virtual_no];
+    } else {
+        no = VM_NO_ASSIGN;
+    }
+
+    return no;
 }
 

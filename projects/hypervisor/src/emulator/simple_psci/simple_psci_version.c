b2f690d5625dd4fda702342905bab424c7029f00
Hidekazu Kato <hkato.z@gmail.com>
2024-08-29 16:34:33 +0900

    PSCI EmulationにVERSION機能追加
    Versionは0.2とする。

---
+++ projects/hypervisor/src/emulator/simple_psci/simple_psci_version.c
@@ -0,0 +1,28 @@
+/*
+ * emulator/simple_psci/simple_psci_version.c
+ *
+ * (C) 2024 Hidekazu Kato
+ */
+
+#include "hypervisor/vpc.h"
+#include "lib/system/errno.h"
+#include <stdint.h>
+
+/* defines */
+
+#define EMULATE_VERSION 0x0002  /* 0.2 */
+
+/* types */
+
+/* prototypes */
+
+/* variables */
+
+/* functions */
+
+errno_t simple_psci_version(struct vpc *vpc)
+{
+    vpc->regs[VPC_X0] = EMULATE_VERSION;
+
+    return SUCCESS;
+}

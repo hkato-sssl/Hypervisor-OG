b9b90fabd661ad5780822034d91be35b40bdec06
Hidekazu Kato <hkato.z@gmail.com>
2024-09-19 20:44:55 +0900

    GICD_SGIRのfilter値の判定を追加
    未サポートのfilter値の場合はエラーメッセージを出力。

--- projects/hypervisor/src/emulator/vgic400/vgic400_distributor_sgir.c
+++ projects/hypervisor/src/emulator/vgic400/vgic400_distributor_sgir.c
@@ -9,6 +9,7 @@
 #include "hypervisor/vpc.h"
 #include "lib/bit.h"
 #include "lib/system/errno.h"
+#include "lib/system/printk.h"
 #include "vgic400_local.h"
 #include <stdint.h>
 
@@ -29,19 +30,27 @@ static errno_t write_sgir(struct vgic400 *vgic, const struct insn *insn)
     uint64_t irq;
     uint64_t v_target_list;
     uint64_t p_target_list;
+    uint64_t filter;
 
     d = insn_str_src_value(insn);
     irq = d & BITS(3, 0);
-    if (irq < 16) {
+
+    filter = BF_EXTRACT(d, 25, 24);
+    switch (filter) {
+    case 0:
         v_target_list = BF_EXTRACT(d, 23, 16);
         p_target_list = vgic400_v2p_cpu_map_b(v_target_list, insn->vpc->vm);
         if (p_target_list != 0) {
             d = (d & ~(uint32_t)BITS(23, 16)) | (p_target_list << 16);
             VGIC400_WRITE32(insn->op.str.pa, d);
         }
-    }
+        ret = insn_emulate_str(insn);
+        break;
 
-    ret = insn_emulate_str(insn);
+    default:
+        printk("#### Unsupported TragetListFilet(=%u).\n", filter);
+        ret = -ENOTSUP;
+    }
 
     return ret;
 }

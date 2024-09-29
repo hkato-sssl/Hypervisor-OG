da98959ec3afe2c315fd91aeeae7b8916c48bd1d
Hidekazu Kato <hkato.z@gmail.com>
2024-09-29 10:48:20 +0900

    未使用変数を削除

--- projects/hypervisor/src/emulator/vgic400/gic400_dump_virtual_cpuif.c
+++ projects/hypervisor/src/emulator/vgic400/gic400_dump_virtual_cpuif.c
@@ -13,9 +13,9 @@
 
 /* defines */
 
-#define NR_VCPUIFS 8
+#define NR_VCPUIFS         8
 
-#define VCPUIF_REG_BASE(n)  (0x5000 + (n) * 0x200)
+#define VCPUIF_REG_BASE(n) (0x5000 + (n) * 0x200)
 
 /* types */
 
@@ -27,8 +27,6 @@
 
 void gic400_dump_virtual_cpuif_at(struct vgic400 *vgic, uint8_t no)
 {
-    int i;
-
     if (no >= NR_VCPUIFS) {
         printk("%s: The value of no(=%u) is out of range.\n", __func__, no);
         return;
@@ -40,10 +38,12 @@ void gic400_dump_virtual_cpuif_at(struct vgic400 *vgic, uint8_t no)
     printk("   GICV_BPR: 0x%08x\n", gic400_read_virtual_cpuif(vgic, GICV_BPR));
     printk("   GICV_IAR: 0x%08x\n", gic400_read_virtual_cpuif(vgic, GICV_IAR));
     printk("   GICV_RPR: 0x%08x\n", gic400_read_virtual_cpuif(vgic, GICV_RPR));
-    printk(" GICV_HPPIR: 0x%08x\n", gic400_read_virtual_cpuif(vgic, GICV_HPPIR));
+    printk(" GICV_HPPIR: 0x%08x\n",
+           gic400_read_virtual_cpuif(vgic, GICV_HPPIR));
     printk("  GICV_ABPR: 0x%08x\n", gic400_read_virtual_cpuif(vgic, GICV_ABPR));
     printk("  GICV_AIAR: 0x%08x\n", gic400_read_virtual_cpuif(vgic, GICV_AIAR));
-    printk("GICV_AHPPIR: 0x%08x\n", gic400_read_virtual_cpuif(vgic, GICV_AHPPIR));
+    printk("GICV_AHPPIR: 0x%08x\n",
+           gic400_read_virtual_cpuif(vgic, GICV_AHPPIR));
     printk("  GICV_APR0: 0x%08x\n", gic400_read_virtual_cpuif(vgic, GICV_APR0));
     printk("  GICV_IIDR: 0x%08x\n", gic400_read_virtual_cpuif(vgic, GICV_IIDR));
     printk("\n");

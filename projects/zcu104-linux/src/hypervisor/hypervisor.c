f193f996a02a50af1e3f251ff86336a676b9d10a
Hidekazu Kato <hkato.z@gmail.com>
2024-10-22 11:28:57 +0900

    Guest起動処理のコード改修
    コメントの追加やコードを整理。

--- projects/zcu104-linux/src/hypervisor/hypervisor.c
+++ projects/zcu104-linux/src/hypervisor/hypervisor.c
@@ -36,7 +36,10 @@ extern struct gic400 sys_gic;
 
 /* functions */
 
-static void launch_linux_guest(struct xilinx_mpsoc *chip, uint32_t vpc_no,
+/* Guest Thread：
+ * 各 Thread は割り当てられた Gest 環境上で、１つの仮想プロセッサを起動する。
+ */
+static void linux_guest_thread(struct xilinx_mpsoc *chip, uint32_t vpc_no,
                                uintptr_t boot_addr, uintptr_t dtb_addr)
 {
     errno_t ret;
@@ -54,60 +57,66 @@ static void launch_linux_guest(struct xilinx_mpsoc *chip, uint32_t vpc_no,
         printk("vm_launch_secondary() -> %d\n", ret);
     }
 
+    /* The following code will be executed only if an error occurs. */
+    
     vpc_dump(chip->soc.vm.vpcs[vpc_no], 0);
 }
 
+static void launch_linux_thread(const char *name, int proc_no,
+                                struct xilinx_mpsoc *chip, int vpc_no)
+{
+    struct thread_parameter parameter;
+
+    printk("Start a thread for %s: PhysicalProcessor=%d VirtualProcessor=%d\n", name, proc_no, vpc_no);
+
+    memset(&parameter, 0, sizeof(parameter));
+    parameter.entry = (thread_entry_t)linux_guest_thread;
+    parameter.args[0] = (uintptr_t)chip;
+    parameter.args[1] = vpc_no;
+    parameter.args[2] = BOOT_ADDR;
+    parameter.args[3] = DTB_ADDR;
+    parameter.args[4] = (uintptr_t)name;
+
+    if (proc_no == aarch64_cpu_no()) {
+        thread_launch_self(&parameter);
+    } else {
+        thread_launch(proc_no, &parameter);
+    }
+}
+
+/* Hypervisor thread 起動処理：
+ * Guest 上の仮想プロセッサ毎に Thread を１つ起動する。
+ */
 void hypervisor_main(void)
 {
     struct xilinx_mpsoc *chip;
-    struct thread_parameter parameter;
-
-    printk("<%u#%s>\n", aarch64_cpu_no(), __func__);
 
     gic400_enable_interrupt(&sys_gic, IRQ_SMMU500);
     gic400_set_priority_mask(&sys_gic, 0xff);
 
     guest_initialize_shared_resource();
 
-    memset(&parameter, 0, sizeof(parameter));
-    parameter.entry = (thread_entry_t)launch_linux_guest;
-    parameter.args[2] = BOOT_ADDR;
-    parameter.args[3] = DTB_ADDR;
-
-#if 1
     chip = guest_usb_linux();
-    parameter.args[0] = (uintptr_t)chip;
-    parameter.args[1] = 1;
-    thread_launch(2, &parameter);
+    launch_linux_thread("Linux#1", 1, chip, 1);
+    launch_linux_thread("Linux#1", 3, chip, 0);
 
-    parameter.args[1] = 0;
-    thread_launch(3, &parameter);
-#endif
-
-#if 1
     chip = guest_linux();
-    parameter.args[0] = (uintptr_t)chip;
-    parameter.args[1] = 1;
-    thread_launch(1, &parameter);
-
-    parameter.args[1] = 0;
-    thread_launch_self(&parameter);
-#endif
+    launch_linux_thread("Linux#0", 2, chip, 1);
+    launch_linux_thread("Linux#0", 0, chip, 0);
 }
 
 void hypervisor(void)
 {
-    /* NOTE:
-     * Hypervisor 起動時は Primary Processor により本関数が最初に呼ばれる。
+    /* Hypervisor 起動処理：
+     * 本関数でシステム上で起動される最初の Thread を生成する。
+     * システム 起動時は Primary Processor により本関数が最初に呼ばれる。
      * この時、全ての Secondary Processor は Standby 状態にある。
-     * 本関数実行時は thread としては起動していないので TLS へアクセスする
-     * API は実行不可能。
-     * 本関数の役割は H/W に関する初期化処理や thread の生成となる。
      */
 
     struct thread_parameter parameter;
 
-    printk("<%s>\n", __func__);
+    printk("Start a startup thread.\n");
+
     memset(&parameter, 0, sizeof(parameter));
     parameter.entry = (thread_entry_t)hypervisor_main;
     thread_launch_self(&parameter);
